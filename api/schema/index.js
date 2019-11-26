20af84e4e07ae9bd62357dd97d4c971df365731b
Gustav Larsson <gustav.larsson@gmail.com>
2019-11-26 16:33:11 +0100

    Move models, resolvers, schema into subfolder

--- api/schema/index.js
+++ api/schema/index.js
@@ -1,3 +1,5 @@
+import { gql } from 'apollo-server';
+
 const schema = gql`
   type Query {
     user(id: ID!): User
@@ -32,7 +34,20 @@ const schema = gql`
     title: String!
     description: String
     members: [Membership!]
+    dreams: [Dream!]
   }
+
+  type Dream {
+    id: ID!
+    slug: String!
+    title: String!
+    description: String
+    images: [String!]
+    team: [Membership!]!
+    budget: String
+    minGrant: Int
+  }
+  # type Image {}
 `;
 
 export default schema;
