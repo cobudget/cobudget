fdb8fcc45d2b8fb3c4c5e34f5270f156c3acdf0f
Gustav Larsson <gustav.larsson@gmail.com>
2019-12-03 11:38:57 +0100

    Update schema definition

--- api/schema/index.js
+++ api/schema/index.js
@@ -2,8 +2,6 @@ import { gql } from 'apollo-server';
 
 const schema = gql`
   type Query {
-    user(id: ID!): User
-    users: [User!]
     currentUser: User
     events: [Event!]
     event(slug: String!): Event
@@ -13,30 +11,15 @@ const schema = gql`
   type Mutation {
     createEvent(slug: String!, title: String!, description: String): Event!
     createDream(
-      eventSlug: String!
+      eventId: ID!
       title: String!
       description: String
-      budget: String
-      minGrant: Int
+      budgetDescription: String
+      minFunding: Int
     ): Dream
+    # sendMagicLink: Boolean
     createUser(name: String, email: String): User # hack
-    dropStuff: Boolean
-  }
-
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    memberships: [Membership!]
-  }
-
-  # type Email {}
-
-  type Membership {
-    id: ID!
-    user: User!
-    event: Event!
-    isAdmin: Boolean!
+    # dropStuff: Boolean
   }
 
   type Event {
@@ -44,8 +27,41 @@ const schema = gql`
     slug: String!
     title: String!
     description: String
-    members: [Membership!]
+    # logo: String
+    memberships: [Membership!]
     dreams: [Dream!]
+    # flags: [Flag!]
+    # questions: [Question!]
+    # reactions: [Emoji] #requried or not? ui implications?
+    # visibility: Visibility
+    # registrationPolicy: RegistrationPolicy
+    # grantingPeriods: [GrantingPeriod]
+    # currency: Currency! # scalar? # can't change after first submission closes
+    # useGrantlings: Boolean! # can't change after first submission close
+    # membershipContribution: Int
+    # grantlingValue: Int # can't change after first submission close?
+    # totalBudget: Int
+    # amountLeft: Int
+    # grantlingValue: Int
+  }
+
+  type User {
+    id: ID!
+    email: String! # emailVerified create Email type for keeping track of email verification etc?
+    memberships: [Membership]
+    name: String
+    avatar: String
+  }
+
+  type Membership {
+    id: ID!
+    event: Event!
+    user: User!
+    # isActive: Boolean!
+    # isApproved: Boolean!
+    isAdmin: Boolean!
+    # isGuide: Boolean!
+    # favorites: [Dream]
   }
 
   type Dream {
@@ -53,11 +69,95 @@ const schema = gql`
     slug: String!
     title: String!
     description: String
-    images: [String!]
-    team: [User!]!
-    budget: String
-    minGrant: Int
+    event: Event
+    images: [String]
+    team: [User]!
+    minFunding: Int #real currency
+    maxFunding: Int
+    budgetDescription: String
+    # isApprovedForGranting: Boolean # should this be per granting period?
+    # answers: [QuestionAnswer]
+    # funding: Int!
+    # raisedFlags: [Flag]
+    # reactions: [Reaction]
+    # tags: [Tag]
   }
+
+  # enum Visibility {
+  #   PUBLIC
+  #   PRIVATE # only for paid
+  #   HIDDEN # only for paid
+  # }
+
+  # enum RegistrationPolicy {
+  #   OPEN
+  #   REQUEST_TO_JOIN
+  #   INVITE_ONLY
+  # }
+
+  # type GrantingPeriod {
+  #   event: Event!
+  #   submissionCloses: Date
+  #   grantingStarts: Date
+  #   grantingCloses: Date # when this happens. all grants to non-funded projects go back into the pool
+  #   name: String
+  #   budget: Int
+  #   distributeGrants: DistributeGrantStrategy
+  # }
+
+  # enum DistributeGrantStrategy {
+  #   DISTRIBUTE_EQUALLY # is this possible?
+  #   DISTRIBUTE_TO_ACTIVE_USERS
+  #   COMITTEE
+  # }
+
+  # type Emoji {
+  #   unicode: String!
+  #   event: Event!
+  # }
+
+  # type Reaction {
+  #   emoji: Emoji!
+  #   by: Member!
+  #   # createdAt
+  # }
+
+  # type Comment {
+  #   dream: Dream!
+  #   by: Member!
+  #   createdAt: Date!
+  # }
+
+  # type Flag {
+  #   title: String!
+  #   description: String!
+  # }
+
+  # type FlagEvent {
+  #   flag: Flag!
+  #   flagger: Member!
+  # }
+
+  # type Favorite {
+  #   dream: Dream!
+  #   by: Member!
+  # }
+
+  # type Grant {
+  #   dream: Dream!
+  #   amount: Int!
+  # }
+
+  # type QuestionAnswer {
+  #   question: Question
+  #   answer: String
+  # }
+
+  # type Question {
+  #   richtext: String
+  #   isRequired: Boolean!
+  # }
+
   # type Image {}
 `;
 
