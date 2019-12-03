370dca4ed3493e026b5d15f7b6bafa88e1bdba39
Gustav Larsson <gustav.larsson@gmail.com>
2019-12-03 16:57:39 +0100

    Add unique compound indexes to eventId and slug on Dreams and to userId and eventId on memberships

--- api/models/index.js
+++ api/models/index.js
@@ -1,68 +1,67 @@
 import mongoose, { Schema } from 'mongoose';
 
-export const User = mongoose.model(
-  'User',
-  new Schema({
-    name: String,
-    email: {
-      type: String,
-      required: true,
-      index: true,
-      unique: true
-    }
-    // created_at: {
-    //   type: Date,
-    //   default: Date.now,
-    // },
-  })
-);
+// User
+const UserSchema = new Schema({
+  name: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }
+  // created_at: {
+  //   type: Date,
+  //   default: Date.now,
+  // },
+});
 
-export const Membership = mongoose.model(
-  'Membership',
-  new Schema({
-    userId: {
-      type: Schema.Types.ObjectId,
-      index: true,
-      required: true
-    },
-    eventId: {
-      type: Schema.Types.ObjectId,
-      index: true,
-      required: true
-    },
-    isAdmin: { type: Boolean, required: true, default: false }
-    // guide
-    // joined?
-    // ticket?
-  })
-);
+export const User = mongoose.model('User', UserSchema);
 
-export const Event = mongoose.model(
-  'Event',
-  new Schema({
-    slug: {
-      type: String,
-      required: true,
-      index: true,
-      unique: true
-    },
-    title: { type: String, required: true },
-    description: String
-  })
-);
+// Membership
+const MembershipSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    index: true,
+    required: true
+  },
+  eventId: {
+    type: Schema.Types.ObjectId,
+    index: true,
+    required: true
+  },
+  isAdmin: { type: Boolean, required: true, default: false }
+  // guide
+  // joined?
+  // ticket?
+}).index({ userId: 1, eventId: 1 }, { unique: true });
 
-export const Dream = mongoose.model(
-  'Dream',
-  new Schema({
-    eventId: { type: Schema.Types.ObjectId, required: true, index: true },
-    slug: { type: String, required: true, index: true },
-    title: { type: String, required: true },
-    description: String,
-    teamIds: [Schema.Types.ObjectId],
-    budgetDescription: String,
-    minFunding: Number
-  })
-);
+export const Membership = mongoose.model('Membership', MembershipSchema);
+
+// Event
+const EventSchema = new Schema({
+  slug: {
+    type: String,
+    required: true,
+    index: true,
+    unique: true
+  },
+  title: { type: String, required: true },
+  description: String
+});
+
+export const Event = mongoose.model('Event', EventSchema);
+
+// Dream
+const DreamSchema = new Schema({
+  eventId: { type: Schema.Types.ObjectId, required: true },
+  slug: { type: String, required: true },
+  title: { type: String, required: true },
+  description: String,
+  teamIds: [Schema.Types.ObjectId],
+  budgetDescription: String,
+  minFunding: Number
+}).index({ eventId: 1, slug: 1 }, { unique: true });
+
+export const Dream = mongoose.model('Dream', DreamSchema);
 
 export default {
   User,
