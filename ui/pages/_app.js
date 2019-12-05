6db9758d4f3e5100e197e45eb824306b034e9fc3
Gustav Larsson <gustav.larsson@gmail.com>
2019-12-05 23:41:40 +0100

    UI architecture: simplify _app to only query currentUser

--- ui/pages/_app.js
+++ ui/pages/_app.js
@@ -2,53 +2,32 @@ import React from "react";
 import App from "next/app";
 import { ApolloProvider } from "@apollo/react-hooks";
 import gql from "graphql-tag";
-import getHostInfo from "../utils/getHostInfo";
 import withData from "../utils/apolloClient";
-
-const EVENT_QUERY = gql`
-  query Event($slug: String!) {
-    event(slug: $slug) {
-      slug
-      id
-      description
-      title
-      dreams {
-        title
-        slug
-      }
-    }
-  }
-`;
 
 const CURRENT_USER_QUERY = gql`
   {
     currentUser {
       name
       email
+      memberships {
+        event {
+          id
+          title
+          slug
+        }
+      }
     }
   }
 `;
 
 class MyApp extends App {
   render() {
-    const {
-      Component,
-      pageProps,
-      apollo,
-      hostInfo,
-      event,
-      currentUser
-    } = this.props;
+    const { Component, pageProps, apollo, currentUser } = this.props;
+
 
     return (
       <ApolloProvider client={apollo}>
-        <Component
-          {...pageProps}
-          hostInfo={hostInfo}
-          event={event}
-          currentUser={currentUser}
-          apollo={apollo}
-        />
+        <Component {...pageProps} currentUser={currentUser} apollo={apollo} />
       </ApolloProvider>
     );
   }
@@ -64,19 +43,7 @@ MyApp.getInitialProps = async appContext => {
     query: CURRENT_USER_QUERY
   });
 
-  const hostInfo = getHostInfo(appContext.ctx.req);
-
-  let event;
-
-  if (hostInfo.subdomain) {
-    const { data } = await appContext.ctx.apolloClient.query({
-      query: EVENT_QUERY,
-      variables: { slug: hostInfo.subdomain }
-    });
-    ({ event } = data);
-  }
-
-  return { ...appProps, hostInfo, event, currentUser };
+  return { ...appProps, currentUser };
 };
 // Wraps all components in the tree with the data provider
 export default withData(MyApp);
