992f3cf4589b81b6d1c24d32616e6bf88c9ca0f0
Gustav Larsson <gustav.larsson@gmail.com>
2019-12-11 20:30:00 +0100

    UI: Add edit dream link if you are a member of that dream

--- ui/pages/[dream]/index.js
+++ ui/pages/[dream]/index.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import Link from "next/link";
 import Card from "../../components/styled/Card";
 import stringToHslColor from "../../utils/stringToHslColor";
+import { isMemberOfDream } from "../../utils/helpers";
 
 // confusing naming, conflicting with other component.
 const DreamCard = styled(Card)`
@@ -14,6 +15,18 @@ const DreamCard = styled(Card)`
   }
   p {
     white-space: pre-line;
+  }
+  .flex {
+    display: flex;
+  }
+  .main {
+    flex: 0 1 70%;
+  }
+  .sidebar {
+    flex: 0 1 30%;
+    /* background: #f1f2f3;
+    border-radius: 8px;
+    padding: 15px; */
   }
 `;
 
@@ -26,6 +39,9 @@ export const DREAM_QUERY = gql`
       title
       minGoal
       maxGoal
+      members {
+        id
+      }
       images {
         small
         large
@@ -40,18 +56,16 @@ const ImgPlaceholder = styled.div`
   height: 250px;
 `;
 
-const Dream = ({ event }) => {
+const Dream = ({ event, currentUser }) => {
   if (!event) return null;
   const router = useRouter();
-  console.log({ routerInFunctionalComp: router });
+
   const { data: { dream } = { dream: null }, loading, error } = useQuery(
     DREAM_QUERY,
     {
       variables: { slug: router.query.dream, eventId: event.id }
     }
   );
-
-  console.log({ dream });
 
   return (
     <DreamCard>
@@ -63,20 +77,35 @@ const Dream = ({ event }) => {
         ))}
       <div>
         <h1>{dream && dream.title}</h1>
-        <p>{dream && dream.description}</p>
+        <div className="flex">
+          <div className="main">
+            <p>{dream && dream.description}</p>
+          </div>
+          <div className="sidebar">
+            {dream && (
+              <>
+                {dream.minGoal && (
+                  <h3>
+                    Min goal: {dream.minGoal} {event.currency}
+                  </h3>
+                )}
+                {dream.maxGoal && (
+                  <h3>
+                    Max goal: {dream.maxGoal} {event.currency}
+                  </h3>
+                )}
+                {isMemberOfDream(currentUser, dream) && (
+                  <Link href="/[dream]/edit" as={`/${dream.slug}/edit`}>
+                    <a>Edit dream</a>
+                  </Link>
+                )}
+              </>
+            )}
+          </div>
+        </div>
       </div>
-      {dream && (
-        <Link href="/[dream]/edit" as={`/${dream.slug}/edit`}>
-          Edit
-        </Link>
-      )}
     </DreamCard>
   );
 };
-
-// Dream.getInitialProps = async (first, second) => {
-//   console.log({ first, second });
-//   return {};
-// };
 
 export default Dream;
--- ui/utils/helpers.js
+++ ui/utils/helpers.js
@@ -4,3 +4,10 @@ export const isMemberOfEvent = (currentUser, event) => {
     if (membership.event.slug === event.slug) return true;
   }, false);
 };
+
+export const isMemberOfDream = (currentUser, dream) => {
+  if (!currentUser || !dream) return false;
+  return dream.members.reduce((res, member) => {
+    if (member.id === currentUser.id) return true;
+  }, false);
+};
