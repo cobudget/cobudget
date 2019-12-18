263a84b8f2a4ce21906d7b41fd0bf37d9363fdb2
Gustav Larsson <gustav.larsson@gmail.com>
2019-12-18 23:45:33 +0100

    UI: Add padding to invite members form

--- ui/components/admin-page/members/InviteMembersForm.js
+++ ui/components/admin-page/members/InviteMembersForm.js
@@ -40,40 +40,47 @@ export default ({ handleClose }) => {
         boxShadow: "none"
       }}
     >
-      <Typography variant="h5">Invite members</Typography>
-      <form
-        onSubmit={handleSubmit(variables => {
-          inviteMembers({ variables }).then(data => {
-            reset();
-            handleClose();
-          });
-        })}
-      >
-        <Box m="15px 0">
-          <TextField
-            id="outlined-multiline-flexible"
-            label="Comma separated emails"
-            multiline
-            fullWidth
-            rows="4"
-            name="emails"
-            error={Boolean(errors.emails)}
-            helperText={errors.emails && errors.emails.message}
-            variant="outlined"
-            inputRef={register({
-              required: "Required",
-              pattern: {
-                value: /^[\W]*([\w+\-.%]+@[\w\-.]+\.[A-Za-z]+[\W]*,{1}[\W]*)*([\w+\-.%]+@[\w\-.]+\.[A-Za-z]+)[\W]*$/,
-                message: "Need to be a comma separated list of emails"
-              }
-            })}
-          />
-        </Box>
+      <Box p={3}>
+        <Typography variant="h5">Invite members</Typography>
+        <form
+          onSubmit={handleSubmit(variables => {
+            inviteMembers({ variables }).then(data => {
+              reset();
+              handleClose();
+            });
+          })}
+        >
+          <Box m="15px 0">
+            <TextField
+              id="outlined-multiline-flexible"
+              label="Comma separated emails"
+              multiline
+              fullWidth
+              rows="4"
+              name="emails"
+              error={Boolean(errors.emails)}
+              helperText={errors.emails && errors.emails.message}
+              variant="outlined"
+              inputRef={register({
+                required: "Required",
+                pattern: {
+                  value: /^[\W]*([\w+\-.%]+@[\w\-.]+\.[A-Za-z]+[\W]*,{1}[\W]*)*([\w+\-.%]+@[\w\-.]+\.[A-Za-z]+)[\W]*$/,
+                  message: "Need to be a comma separated list of emails"
+                }
+              })}
+            />
+          </Box>
 
-        <Button size="large" variant="contained" color="primary" type="submit">
-          Send invites
-        </Button>
-      </form>
+          <Button
+            size="large"
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Send invites
+          </Button>
+        </form>
+      </Box>
     </Card>
   );
 };
