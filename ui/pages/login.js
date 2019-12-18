fb54ff130664d1ec62c464263f91275ce452d090
Gustav Larsson <gustav.larsson@gmail.com>
2019-12-18 22:59:57 +0100

    UI: Show different messages on login page depending on registration policy

--- ui/pages/login.js
+++ ui/pages/login.js
@@ -2,6 +2,7 @@ import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
 import useForm from "react-hook-form";
 import styled from "styled-components";
+import { Box } from "@material-ui/core";
 import Card from "../components/styled/Card";
 import Form from "../components/styled/Form";
 
@@ -29,38 +30,52 @@ export default ({ currentMember, event }) => {
   if (currentMember) {
     return <Card>You are logged in as {currentMember.email}.</Card>;
   }
+  let msg;
+  switch (event.registrationPolicy) {
+    case "OPEN":
+      msg = "Log in or sign up with a magic link";
+      break;
+    case "REQUEST_TO_JOIN":
+      msg = "Log in or request to join";
+      break;
+    case "INVITE_ONLY":
+      msg = "Log in with magic link";
+      break;
+  }
 
   return (
     <SmallCard>
-      <h1>Log in or sign up with magic link</h1>
+      <Box p={3}>
+        <h1>{msg}</h1>
 
-      {data && data.sendMagicLink ? (
-        <div>Magic link sent! Check your inbox!</div>
-      ) : (
-        <Form
-          onSubmit={handleSubmit(({ email }) => {
-            sendMagicLink({ variables: { email, eventId: event.id } });
-          })}
-        >
-          <div className="two-cols-3-1">
-            <input
-              name="email"
-              disabled={loading}
-              placeholder="Email"
-              ref={register({
-                required: "Required",
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                  message: "invalid email address"
-                }
-              })}
-            />
-            <button type="submit">Send</button>
-          </div>
+        {data && data.sendMagicLink ? (
+          <div>Magic link sent! Check your inbox!</div>
+        ) : (
+          <Form
+            onSubmit={handleSubmit(({ email }) => {
+              sendMagicLink({ variables: { email, eventId: event.id } });
+            })}
+          >
+            <div className="two-cols-3-1">
+              <input
+                name="email"
+                disabled={loading}
+                placeholder="Email"
+                ref={register({
+                  required: "Required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: "invalid email address"
+                  }
+                })}
+              />
+              <button type="submit">Send</button>
+            </div>
 
-          {errors.email && email.title.message}
-        </Form>
-      )}
+            {errors.email && email.title.message}
+          </Form>
+        )}
+      </Box>
     </SmallCard>
   );
 };
