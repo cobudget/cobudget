b4bb657d649405cbc5c58474dfdedd34f94527d9
Gustav Larsson <gustav.larsson@gmail.com>
2020-03-17 15:03:28 +0100

    UI: Add loading spinner

---
+++ ui/components/HappySpinner.js
@@ -0,0 +1,5 @@
+import { HappyEmojiIcon } from "./Icons";
+
+export default () => {
+  return <HappyEmojiIcon className="w-12 h-12 text-gray-600 animation-spin" />;
+};
--- ui/components/Icons.js
+++ ui/components/Icons.js
@@ -41,3 +41,14 @@ export const RightArrowIcon = props => (
     ></path>
   </svg>
 );
+
+export const HappyEmojiIcon = props => (
+  <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" {...props}>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+    ></path>
+  </svg>
+);
