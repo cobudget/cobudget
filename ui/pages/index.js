7e678a45cdceb15c5f5db7696070ac9ed513fccc
Gustav Larsson <gustav.larsson@gmail.com>
2020-05-12 11:43:12 +0200

    Move landing page into /pages/index (#36)

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -1,5 +1,34 @@
-import LandingPage from "../components/LandingPage";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import Link from "next/link";
+import { RightArrowIcon } from "../components/Icons";
 
-export default ({ currentMember, event, hostInfo }) => {
-  return <LandingPage hostInfo={hostInfo} />;
+const EVENTS_QUERY = gql`
+  query Events {
+    events {
+      id
+      slug
+      title
+    }
+  }
+`;
+
+export default () => {
+  const { data: { events } = { events: [] } } = useQuery(EVENTS_QUERY);
+  return (
+    <div className="mx-auto mt-2 w-full sm:w-64">
+      <ul className="bg-white rounded-lg shadow-md overflow-hidden">
+        {events.map((event) => (
+          <li key={event.slug} className="border-b last:border-0">
+            <Link href="/[event]" as={`/${event.slug}`}>
+              <a className="group px-4 py-3 block text-lg text-gray-700 hover:bg-gray-100 flex justify-between items-center ">
+                {event.title}
+                <RightArrowIcon className="ml-4 w-4 h-4 text-white group-hover:text-gray-600" />
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
