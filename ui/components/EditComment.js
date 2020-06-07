e72ba291a187702e421f692ce0f231498c891531
Gal Bracha <galbra@gmail.com>
2020-06-07 19:02:54 +0300

    Added more margin for better ui

--- ui/components/EditComment.js
+++ ui/components/EditComment.js
@@ -2,6 +2,7 @@ import React from "react";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
 import { TextField, Button } from "@material-ui/core";
+import Box from '@material-ui/core/Box';
 import { useForm } from "react-hook-form";
 
 const EDIT_COMMENT_MUTATION = gql`
@@ -39,21 +40,23 @@ const EditComment = ({ comment, dreamId, handleDone }) => {
           .catch((err) => alert(err.message));
       })}
     >
-      <TextField
-        name="content"
-        label="Edit comment"
-        variant="outlined"
-        multiline
-        error={Boolean(errors.content)}
-        helperText={errors.content && errors.content.message}
-        fullWidth
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        inputRef={(e) => {
-          register({ required: "Required" });
-          inputRef.current = e;
-        }}
-      />
+      <Box my={2}>
+        <TextField
+          name="content"
+          label="Edit comment"
+          variant="outlined"
+          multiline
+          error={Boolean(errors.content)}
+          helperText={errors.content && errors.content.message}
+          fullWidth
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          inputRef={(e) => {
+            register({ required: "Required" });
+            inputRef.current = e;
+          }}
+        />
+      </Box>
       <Button
         type="submit"
         size="large"
