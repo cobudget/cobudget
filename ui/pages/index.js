ef10509df3377a5391f7f2d7bde76dcb509e70f2
Gustav Larsson <gustav.larsson@gmail.com>
2020-06-10 01:13:57 +0200

    Update look of landing page event links

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -2,6 +2,7 @@ import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import Link from "next/link";
 import { RightArrowIcon } from "../components/Icons";
+import stringToHslColor from "utils/stringToHslColor";
 
 const EVENTS_QUERY = gql`
   query Events {
@@ -16,19 +17,18 @@ const EVENTS_QUERY = gql`
 export default () => {
   const { data: { events } = { events: [] } } = useQuery(EVENTS_QUERY);
   return (
-    <div className="mx-auto mt-2 w-full sm:w-64">
-      <ul className="bg-white rounded-lg shadow-md overflow-hidden">
-        {events.map((event) => (
-          <li key={event.slug} className="border-b last:border-0">
-            <Link href="/[event]" as={`/${event.slug}`}>
-              <a className="group px-4 py-3 block text-lg text-gray-700 hover:bg-gray-100 flex justify-between items-center ">
-                {event.title}
-                <RightArrowIcon className="ml-4 w-4 h-4 text-white group-hover:text-gray-600" />
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="grid grid-cols-4 gap-4">
+      {events.map((event) => (
+        <Link href="/[event]" as={`/${event.slug}`} key={event.slug}>
+          <a
+            className="group p-4 h-32 font-medium rounded-md block text-white flex justify-between items-start transitions-shadows duration-75 hover:shadow-outline "
+            style={{ background: stringToHslColor(event.title, 60, 56) }}
+          >
+            {event.title}
+            <RightArrowIcon className="ml-4 w-4 h-4 text-white" />
+          </a>
+        </Link>
+      ))}
     </div>
   );
 };
