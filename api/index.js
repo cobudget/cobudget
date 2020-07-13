220c2e8381f5e37582a3d95a235aab544b46d0dc
Gustav Larsson <gustav.larsson@gmail.com>
2020-07-13 17:41:36 +0200

    use process.env.PORT if available

--- api/index.js
+++ api/index.js
@@ -77,7 +77,7 @@ app.use(bodyParser.json());
 
 app.post('/pretix', pretixHandler);
 
-app.listen({ port: 4000 }, () =>
+app.listen({ port: process.env.PORT || 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
 
