b3958c9bc65f52af86148b3cb547c50e0ed5572e
Gustav Larsson <gustav.larsson@gmail.com>
2020-08-31 14:39:03 +0200

    fix null event

--- ui/pages/[event]/about.js
+++ ui/pages/[event]/about.js
@@ -1,6 +1,7 @@
 import About from "components/About";
 
 export default ({ event, currentUser }) => {
+  if (!event) return null;
   return (
     <div className="max-w-screen-md flex-1">
       <About event={event} currentUser={currentUser} />
