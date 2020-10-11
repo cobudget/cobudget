59c3a677501efa2966cccb151c1ac1d19f2208be
Gustav Larsson <gustav.larsson@gmail.com>
2020-10-11 21:34:46 +0200

    Fix login email (DEPLOY_URL was undefined)

--- api/services/email.service.js
+++ api/services/email.service.js
@@ -1,23 +1,20 @@
-
 const mailgun = require('mailgun-js')({
   apiKey: process.env.MAILGUN_API_KEY,
   domain: process.env.MAILGUN_DOMAIN,
   host: 'api.eu.mailgun.net',
 });
 
-const DEPLOY_URL = process.env.VERCEL_URL
-  ? process.env.VERCEL_URL
-  : process.env.DEPLOY_URL;
-
 class EmailService {
-
   static async sendMagicLinkEmail(token, organization, user) {
     // send magic link in production, log it in development
     const { subdomain, customDomain } = organization;
     if (process.env.NODE_ENV === 'production') {
-      const domain = customDomain ? `https://${customDomain}` : `https://${subdomain}.${DEPLOY_URL}`;
+      const domain = customDomain
+        ? `https://${customDomain}`
+        : `https://${subdomain}.${process.env.DEPLOY_URL}`;
+
       const url = `${domain}/?token=${token}`;
-      var data = {
+      const data = {
         from: `${process.env.EMAIL_SENDER}`,
         to: user.email,
         subject: 'Login to Dreams',
@@ -36,17 +33,26 @@ class EmailService {
           throw new Error(`Failed to send magic link ${error.message}`);
         });
     } else {
-      const domain = customDomain? `http://${customDomain}` : `http://${subdomain}.localhost:3000`;
+      const domain = customDomain
+        ? `http://${customDomain}`
+        : `http://${subdomain}.localhost:3000`;
       const url = `${domain}/?token=${token}`;
       console.log(`Here is your magic link: ${url}`);
       return true;
     }
-  };
+  }
 
-  static async sendRequestToJoinNotifications(organization, user, event, emails) {
+  static async sendRequestToJoinNotifications(
+    organization,
+    user,
+    event,
+    emails
+  ) {
     const { subdomain, customDomain } = organization;
     if (process.env.NODE_ENV === 'production') {
-      const domain = customDomain ? `https://${customDomain}` : `https://${subdomain}.${DEPLOY_URL}`;
+      const domain = customDomain
+        ? `https://${customDomain}`
+        : `https://${subdomain}.${process.env.DEPLOY_URL}`;
       var data = {
         from: `${process.env.EMAIL_SENDER}`,
         to: emails,
@@ -67,7 +73,7 @@ class EmailService {
     } else {
       console.log('in development, not sending request to join notifications');
     }
-  };
+  }
 }
 
 module.exports = EmailService;
