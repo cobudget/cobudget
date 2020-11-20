467fc86b5ce964ea81e2652ac64b222e9723382e
Gustav Larsson <gustav.larsson@gmail.com>
2020-11-20 18:11:22 +0100

    modify getToken endpoint to not return 500 on missing session (user might simply not be logged in)

--- ui/pages/api/getToken.js
+++ ui/pages/api/getToken.js
@@ -1,13 +1,13 @@
 import auth from "lib/auth";
 
-export default async function session(req, res) {
+export default async function getToken(req, res) {
   try {
-    const { accessToken } = await auth(req).getSession(req);
+    const session = await auth(req).getSession(req);
 
-    if (accessToken) res.send(accessToken);
-    res.status(500).end(accessToken);
+    if (session?.accessToken) res.send(session.accessToken);
+    res.status(204).end();
   } catch (error) {
-    // console.error(error);
+    //console.error(error);
     res.status(error.status || 500).end(error.message);
   }
 }
