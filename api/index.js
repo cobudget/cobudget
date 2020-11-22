bfa51301796dd1832a62f95e82336ac1ecfc5195
Gustav Larsson <gustav.larsson@gmail.com>
2020-11-22 21:00:33 +0100

    get user with keycloak-connect-graphql in api

--- api/index.js
+++ api/index.js
@@ -8,20 +8,6 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const pretixWebhook = require('./webhooks/pretix');
-// const microCors = require('micro-cors');
-// const cors = microCors({
-//   allowHeaders: [
-//     'X-Requested-With',
-//     'Access-Control-Allow-Origin',
-//     'X-HTTP-Method-Override',
-//     'Content-Type',
-//     'Authorization',
-//     'Accept',
-//     'Dreams-Subdomain',
-//     'Dreams-Customdomain',
-//   ],
-// });
-
 const schema = require('./schema');
 const resolvers = require('./resolvers');
 const { getModels } = require('./database/models');
@@ -29,10 +15,34 @@ const {
   db: { getConnection },
 } = require('plato-core');
 
+const Keycloak = require('keycloak-connect');
+const { KeycloakContext } = require('keycloak-connect-graphql');
+
+const app = express();
+const keycloak = new Keycloak(
+  {},
+  {
+    realm: 'plato',
+    'auth-server-url': 'https://auth.platoproject.org/auth/',
+    'ssl-required': 'external',
+    resource: 'dreams',
+    'public-client': true,
+    'confidential-port': 0,
+  }
+);
+
+app.use('/graphql', keycloak.middleware());
+
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
   context: async ({ req }) => {
+    const kauth = new KeycloakContext({ req });
+    console.log({
+      kauth,
+      content: kauth.accessToken && kauth.accessToken.content,
+    });
+
     const db = await getConnection(process.env.MONGO_URL);
     const models = getModels(db);
 
@@ -50,35 +60,35 @@ const server = new ApolloServer({
     let token = req.headers.authorization
       ? req.headers.authorization.split(' ')[1]
       : null;
-
+    console.log({ tokenOnServer: token });
     // Verify token if available
-    if (currentOrg && token) {
-      try {
-        token = jwt.verify(token, process.env.JWT_SECRET);
-        currentUser = await models.User.findOne({
-          _id: token.sub,
-          organizationId: currentOrg.id,
-        });
-      } catch (error) {
-        // throw new AuthenticationError(
-        //   'Authentication token is invalid, please log in.'
-        // );
-        console.error('Authentication token is invalid');
-        console.error(error);
-      }
-    }
+    // if (currentOrg && token) {
+    //   try {
+    //     token = jwt.verify(token, process.env.JWT_SECRET);
+    //     currentUser = await models.User.findOne({
+    //       _id: token.sub,
+    //       organizationId: currentOrg.id,
+    //     });
+    //   } catch (error) {
+    //     // throw new AuthenticationError(
+    //     //   'Authentication token is invalid, please log in.'
+    //     // );
+    //     console.error('Authentication token is invalid');
+    //     console.error(error);
+    //   }
+    // }
 
     return {
       models,
       currentUser,
       currentOrg,
+      // kauth,
     };
   },
   playground: true,
   introspection: true,
 });
 
-const app = express();
 server.applyMiddleware({ app });
 
 app.use(bodyParser.json());
