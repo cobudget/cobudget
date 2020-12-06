f3658cd7e0a366f7c7dd6987b442c2c1fcf47ded
Gustav Larsson <gustav.larsson@gmail.com>
2020-12-06 19:14:04 +0100

    add migration to rename member collection to event member

---
+++ api/database/migrations/20201126215833-rename_member_collection_to_event_member.js
@@ -0,0 +1,17 @@
+module.exports = {
+  async up(db, client) {
+    await db
+      .collection('members')
+      .rename('eventmembers', function (err, newColl) {
+        if (err) console.error(err);
+      });
+  },
+
+  async down(db, client) {
+    await db
+      .collection('eventmembers')
+      .rename('members', function (err, newColl) {
+        if (err) console.error(err);
+      });
+  },
+};
