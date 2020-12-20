c164265640b1d0638ee7a3b2ca212fef15f5e219
Gustav Larsson <gustav.larsson@gmail.com>
2020-12-20 20:53:11 +0100

    use usernames in edit cocreator modal

--- ui/components/Dream/EditCocreatorsModal.js
+++ ui/components/Dream/EditCocreatorsModal.js
@@ -10,10 +10,12 @@ const SEARCH_MEMBERS_QUERY = gql`
     members(eventId: $eventId, isApproved: $isApproved) {
       id
       isApproved
-      user {
-        id
-        name
-        avatar
+      orgMember {
+        user {
+          id
+          username
+          avatar
+        }
       }
     }
   }
@@ -25,10 +27,12 @@ const ADD_CO_CREATOR_MUTATION = gql`
       id
       cocreators {
         id
-        user {
-          id
-          name
-          avatar
+        orgMember {
+          user {
+            id
+            username
+            avatar
+          }
         }
       }
     }
@@ -41,10 +45,12 @@ const REMOVE_CO_CREATOR_MUTATION = gql`
       id
       cocreators {
         id
-        user {
-          id
-          name
-          avatar
+        orgMember {
+          user {
+            id
+            username
+            avatar
+          }
         }
       }
     }
@@ -55,8 +61,10 @@ const Member = ({ member, add, remove }) => {
   return (
     <div className="flex items-center justify-between mb-2 overflow-y-scroll ">
       <div className="flex items-center">
-        <Avatar user={member.user} size="small" />
-        <span className="ml-2 text-gray-800">{member.user.name}</span>
+        <Avatar user={member.orgMember.user} size="small" />
+        <span className="ml-2 text-gray-800">
+          {member.orgMember.user.username}
+        </span>
       </div>
       <div className="flex items-center">
         {Boolean(add) && (
@@ -100,7 +108,9 @@ const SearchMembersResult = ({
 
   if (searchInput) {
     result = result.filter((member) =>
-      member.user.name?.toLowerCase().includes(searchInput.toLowerCase())
+      member.orgMember.user.username
+        ?.toLowerCase()
+        .includes(searchInput.toLowerCase())
     );
   }
 
