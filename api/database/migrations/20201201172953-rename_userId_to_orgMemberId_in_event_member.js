d679a247ffa267101d641a6b2a464d1421b35e58
Gustav Larsson <gustav.larsson@gmail.com>
2021-01-13 13:52:25 +0100

    update old migration

--- api/database/migrations/20201201172953-rename_userId_to_orgMemberId_in_event_member.js
+++ api/database/migrations/20201201172953-rename_userId_to_orgMemberId_in_event_member.js
@@ -2,16 +2,7 @@ module.exports = {
   async up(db, client) {
     await db
       .collection('eventmembers')
-      .update(
-        {},
-        { $rename: { userId: 'orgMemberId' } },
-        { multi: true },
-        function (err, blocks) {
-          if (err) {
-            throw err;
-          }
-        }
-      );
+      .updateMany({}, { $rename: { userId: 'orgMemberId' } });
     // TODO write your migration here.
     // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
     // Example:
@@ -21,16 +12,7 @@ module.exports = {
   async down(db, client) {
     await db
       .collection('eventmembers')
-      .update(
-        {},
-        { $rename: { orgMemberId: 'userId' } },
-        { multi: true },
-        function (err, blocks) {
-          if (err) {
-            throw err;
-          }
-        }
-      );
+      .updateMany({}, { $rename: { orgMemberId: 'userId' } });
     // TODO write the statements to rollback your migration (if possible)
     // Example:
     // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
