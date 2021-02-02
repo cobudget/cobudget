1d285a57e5de46342f384e52ff4bd54962815a1f
Gustav Larsson <gustav.larsson@gmail.com>
2021-02-02 23:58:42 +0100

    add message if not logged in on create organization page

--- ui/pages/organizations/create.js
+++ ui/pages/organizations/create.js
@@ -1,5 +1,11 @@
 import EditOrganization from "components/Organizations/EditOrganization";
 
-export default ({currentUser}) => {
-  return EditOrganization({currentUser});
-}
+export default ({ currentUser }) => {
+  if (!currentUser)
+    return (
+      <h1 className="flex-grow flex justify-center items-center text-xl">
+        You need to log in to create an organization.
+      </h1>
+    );
+  return EditOrganization({ currentUser });
+};
