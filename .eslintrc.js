97d2e58fb5c97718a5b188a2c1578867b8f01e77
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-03-05 18:00:12 +0100

    Improve eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,22 +6,36 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
+        "plugin:react/recommended",
         "prettier",
     ],
     "globals": {
         "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+        "SharedArrayBuffer": "readonly",
+        "React": "readonly",
     },
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
         },
-        "ecmaVersion": 2018,
+        "ecmaVersion": 2020,
         "sourceType": "module"
     },
     "plugins": [
         "react"
     ],
     "rules": {
+        // not having to import 'React' in every file to use jsx (next does that)
+        "react/react-in-jsx-scope": "off",
+        // don't require components to define prop types
+        "react/prop-types": "off",
+        // don't require components to have a display name
+        "react/display-name": "off",
+        // allows to use globals like 'React' in jsx
+        "react/jsx-no-undef": ["error",
+            {
+                "allowGlobals": true,
+            }
+        ],
     }
 };
