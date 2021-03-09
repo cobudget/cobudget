950048499b372c757ad7d3ab918e55a5db65515b
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-03-09 14:36:40 +0100

    Add react hook plugin to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
