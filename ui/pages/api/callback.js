fc346b2f7b053e9d3a00f5ab93588424d8f791ce
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-03-12 16:21:55 +0100

    Switch auth redirect from being stored in query string to cookies

--- ui/pages/api/callback.js
+++ ui/pages/api/callback.js
@@ -1,27 +1,15 @@
+import { parseCookies, destroyCookie } from "nookies";
 import auth from "lib/auth";
 
 export default async function callback(req, res) {
-  let redirectTo;
+  const cookies = parseCookies({ req });
+  const redirectTo = cookies.authRedir ?? "/";
 
-  const {
-    query: { redirectPath },
-  } = req;
-
-  // build a path to redirect to from query parameter(s) `redirectPath`
-  if (Array.isArray(redirectPath)) {
-    redirectTo = redirectPath.reduce(
-      (str, curr) => str + "/" + decodeURIComponent(curr),
-      ""
-    );
-
-    // if it is not an array, it is a string (only one part)
-  } else if (redirectPath) {
-    redirectTo = `/${redirectPath}`;
-  }
+  destroyCookie({ res }, "authRedir");
 
   try {
-    await auth(req, redirectPath).handleCallback(req, res, {
-      redirectTo: redirectTo ?? "/",
+    await auth(req).handleCallback(req, res, {
+      redirectTo,
     });
   } catch (error) {
     console.error(error);
