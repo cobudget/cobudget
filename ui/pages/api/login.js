d36990061d034c1106189fa0beb92569833b5144
Gustav Larsson <gustav.larsson@gmail.com>
2021-03-18 15:34:32 +0100

    Use native returnTo logic in auth library (to redirect back to where you were)

--- ui/pages/api/login.js
+++ ui/pages/api/login.js
@@ -1,18 +1,14 @@
-import { setCookie } from "nookies";
 import auth from "lib/auth";
 import getHostInfo from "utils/getHostInfo";
 
 export default async function login(req, res) {
   const { host } = getHostInfo(req);
-  if (req?.headers?.referer) {
-    const path = req.headers.referer.split(host)[1];
+  const path = req.headers.referer.split(host)[1];
 
-    setCookie({ res }, "authRedir", path, {
-      maxAge: 60 * 60, // one hour
-    });
-  }
   try {
-    await auth(req).handleLogin(req, res);
+    await auth(req).handleLogin(req, res, {
+      returnTo: path,
+    });
   } catch (error) {
     console.error(error);
     res.status(error.status || 500).end(error.message);
