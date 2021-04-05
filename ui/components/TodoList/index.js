44f69c0dac9d2dd3e39c81ffcccb9f3d96390b4a
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-04-05 16:36:21 +0200

    Add an unstyled static todolist

--- ui/components/TodoList/index.js
+++ ui/components/TodoList/index.js
@@ -1,10 +1,40 @@
+const todos = [
+  {
+    done: true,
+    title: "Create community",
+    desc:
+      "This is your own home on the Plato platform, now available under not-here.platoproject.org",
+    link: "potato.com",
+  },
+  {
+    done: false,
+    title: "Invite members",
+    desc: "Invite your community members by email",
+    link: "/org/settings?tab=members",
+  },
+  {
+    done: false,
+    title: "Create first event",
+    desc: "An event is a page for gathering ideas from the community.",
+    link: "/create-event",
+  },
+];
+
 const TodoList = () => {
   return (
     <div className="bg-white rounded-lg shadow p-6 max-w-md">
       <h1 className="text-2xl font-semibold mb-4 text-center">
         {"👌 Let's get this ball rolling!"}
       </h1>
-      <div>put the todolist here</div>
+      <div>progress bar here</div>
+      <div>
+        {todos.map((todo, index) => (
+          <div key={index}>
+            <div>{todo.title}</div>
+            <div>{todo.desc}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
