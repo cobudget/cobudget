2b1283ce16955a0f20ab4a8b23f3e5eb7182471c
Gustav Larsson <gustav.larsson@gmail.com>
2021-04-14 16:28:09 +0200

    Update profile dropdown to display balance instead of availableGrants

--- ui/components/Header/index.js
+++ ui/components/Header/index.js
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import Link from "next/link";
-import { Badge } from "@material-ui/core";
-import { useRouter } from "next/router";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
 
+import thousandSeparator from "utils/thousandSeparator";
 import ProfileDropdown from "components/ProfileDropdown";
 import Avatar from "components/Avatar";
 import { modals } from "components/Modal/index";
@@ -213,31 +212,11 @@ const Header = ({
           {currentOrgMember && (
             <div className="pt-4 pb-1 sm:hidden bg-white mb-4 border-gray-300">
               <div className="flex items-center px-3">
-                <Badge
-                  overlap="circle"
-                  anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "right",
-                  }}
-                  badgeContent={
-                    currentOrgMember.currentEventMembership &&
-                    currentOrgMember.currentEventMembership.availableGrants
-                  }
-                  color="primary"
-                >
-                  <Avatar user={currentOrgMember.user} />
-                </Badge>
+                <Avatar user={currentOrgMember.user} />
                 <div className="ml-4">
                   <span className="font-semibold text-gray-600">
                     {currentOrgMember.user.name}
                   </span>
-                  {/* {currentOrgMember.currentEventMembership &&
-                  Boolean(currentOrgMember.currentEventMembership.availableGrants) && (
-                    <span className="block text-sm text-gray-600">
-                      You have {currentOrgMember.currentEventMembership.availableGrants} tokens
-                      left
-                    </span>
-                  )} */}
                 </div>
               </div>
 
@@ -252,15 +231,18 @@ const Header = ({
                   <div className="mx-2 px-2 py-1 rounded-lg bg-gray-200 mb-1 text-gray-800">
                     {currentOrgMember.currentEventMembership.event.title}
                     {Boolean(
-                      currentOrgMember.currentEventMembership.availableGrants
+                      currentOrgMember.currentEventMembership.balance
                     ) && (
                       <p className=" text-gray-800 text-sm">
                         You have{" "}
-                        {
-                          currentOrgMember.currentEventMembership
-                            .availableGrants
-                        }{" "}
-                        tokens left
+                        <span className="text-black font-medium">
+                          {thousandSeparator(
+                            currentOrgMember.currentEventMembership.balance /
+                              100
+                          )}{" "}
+                          {event.currency}
+                        </span>{" "}
+                        to contribute
                       </p>
                     )}
                   </div>
