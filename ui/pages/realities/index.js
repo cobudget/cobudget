b82eb9b7a3c6c87aca78f1fd8404c8e5644ff6e7
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-04-23 19:40:07 +0200

    Simplify skipping of ssr

--- ui/pages/realities/index.js
+++ ui/pages/realities/index.js
@@ -2,8 +2,9 @@ import React from "react";
 
 import Realities from "components/Realities";
 import SubMenu from "components/SubMenu";
+import HappySpinner from "components/HappySpinner";
 
-const RealitiesPage = ({ currentOrgMember }) => {
+const RealitiesPageClientSide = ({ currentOrgMember }) => {
   return (
     <>
       <SubMenu currentOrgMember={currentOrgMember} />
@@ -12,6 +13,14 @@ const RealitiesPage = ({ currentOrgMember }) => {
       </div>
     </>
   );
+};
+
+const RealitiesPage = () => {
+  const onServer = typeof window === "undefined";
+  // until we've fixed ssr for the realities apollo client we'll just skip ssr here
+  if (onServer) return <HappySpinner />;
+
+  return <RealitiesPageClientSide />;
 };
 
 export default RealitiesPage;
