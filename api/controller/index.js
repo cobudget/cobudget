587ac2ed0b5d5eafa0c68050d78ef4799ca8e95f
Gustav Larsson <gustav.larsson@gmail.com>
2021-04-29 19:14:10 +0200

    Prevent setting negative balances when allocating funds

--- api/controller/index.js
+++ api/controller/index.js
@@ -4,40 +4,42 @@ const allocateToMember = async (
   { eventMemberId, eventId, organizationId, amount, type },
   { Allocation, Contribution }
 ) => {
+  const [
+    { totalAllocations } = { totalAllocations: 0 },
+  ] = await Allocation.aggregate([
+    {
+      $match: {
+        eventMemberId: mongoose.Types.ObjectId(eventMemberId),
+      },
+    },
+    { $group: { _id: null, totalAllocations: { $sum: "$amount" } } },
+  ]);
+
+  const [
+    { totalContributions } = { totalContributions: 0 },
+  ] = await Contribution.aggregate([
+    {
+      $match: {
+        eventMemberId: mongoose.Types.ObjectId(eventMemberId),
+      },
+    },
+    { $group: { _id: null, totalContributions: { $sum: "$amount" } } },
+  ]);
+
+  const balance = totalAllocations - totalContributions;
+
   if (type === "ADD") {
+    const adjustedAmount = balance + amount >= 0 ? amount : -balance;
     await new Allocation({
       organizationId,
       eventId,
       eventMemberId,
-      amount,
+      amount: adjustedAmount,
     }).save();
   } else if (type === "SET") {
-    const [
-      { totalAllocations } = { totalAllocations: 0 },
-    ] = await Allocation.aggregate([
-      {
-        $match: {
-          eventMemberId: mongoose.Types.ObjectId(eventMemberId),
-        },
-      },
-      { $group: { _id: null, totalAllocations: { $sum: "$amount" } } },
-    ]);
-
-    const [
-      { totalContributions } = { totalContributions: 0 },
-    ] = await Contribution.aggregate([
-      {
-        $match: {
-          eventMemberId: mongoose.Types.ObjectId(eventMemberId),
-        },
-      },
-      { $group: { _id: null, totalContributions: { $sum: "$amount" } } },
-    ]);
-
-    const balance = totalAllocations - totalContributions;
+    if (amount < 0) throw new Error("Can't set negative values");
 
     const adjustment = amount - balance;
-
     await new Allocation({
       organizationId,
       eventId,
