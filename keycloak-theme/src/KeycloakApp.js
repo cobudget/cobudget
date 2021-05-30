3a2d26bbe7983ad1d433e75213b71596ef902d53
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-05-30 16:00:56 +0200

    Proof of concept replacing certain keycloak pages

--- keycloak-theme/src/KeycloakApp.js
+++ keycloak-theme/src/KeycloakApp.js
@@ -2,21 +2,55 @@ import React from "react";
 import { Button } from "@material-ui/core";
 import { KcApp, defaultKcProps, kcContext } from "keycloakify";
 
+const DefaultApp = ({ ctx }) => {
+  return (
+    <KcApp
+      kcContext={ctx}
+      {...{
+        ...defaultKcProps,
+        // This injects the class properly but index.css doesn't seem to be
+        // able to get imported, at least in storybook
+        kcHeaderWrapperClass: "top-class",
+      }}
+    />
+  );
+};
+
+const Login = ({ ctx }) => {
+  return (
+    <Button variant="contained" color="primary">
+      Primary
+    </Button>
+  );
+};
+
 const KeycloakApp = ({ mock }) => {
+  const ctx = mock ?? kcContext;
+
   return (
     <React.StrictMode>
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
-      <KcApp
-        kcContext={mock ?? kcContext}
-        {...{
-          ...defaultKcProps,
-          // This injects the class properly but index.css doesn't seem to be
-          // able to get imported, at least in storybook
-          kcHeaderWrapperClass: "top-class",
-        }}
-      />
+      {(() => {
+        switch (ctx.pageId) {
+          case "login.ftl":
+            return <Login ctx={ctx} />;
+          //case "register.ftl":
+          //  return <Register {...{ kcContext, ...props }} />;
+          //case "info.ftl":
+          //  return <Info {...{ kcContext, ...props }} />;
+          //case "error.ftl":
+          //  return <Error {...{ kcContext, ...props }} />;
+          //case "login-reset-password.ftl":
+          //  return <LoginResetPassword {...{ kcContext, ...props }} />;
+          //case "login-verify-email.ftl":
+          //  return <LoginVerifyEmail {...{ kcContext, ...props }} />;
+          //case "terms.ftl":
+          //  return <Terms {...{ kcContext, ...props }} />;
+          //case "login-otp.ftl":
+          //  return <LoginOtp {...{ kcContext, ...props }} />;
+          default:
+            return <DefaultApp ctx={ctx} />;
+        }
+      })()}
     </React.StrictMode>
   );
 };
