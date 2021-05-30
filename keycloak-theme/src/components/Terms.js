cfca11aff14a37eb655bbd5dfb1ed9976d5e6d05
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-05-30 18:09:01 +0200

    Do some basic styling to the terms page

--- keycloak-theme/src/components/Terms.js
+++ keycloak-theme/src/components/Terms.js
@@ -1,32 +1,60 @@
+import { Button } from "@material-ui/core";
+
 const Terms = ({ ctx }) => {
   const { url } = ctx;
   return (
-    <div>
-      <div className="text-3xl text-red-600">
-        Please accept the Privacy Policy and Terms and Conditions
+    <div className="flex">
+      <div className="m-auto bg-gray-200 p-6 mt-10 rounded-2xl">
+        <div className="text-3xl mb-12">
+          Please accept the Privacy Policy and Terms and Conditions
+        </div>
+        <div className="mb-12">
+          Please go to the following links to read the{" "}
+          <a
+            href="https://www.iubenda.com/privacy-policy/58637640"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Privacy Policy
+          </a>{" "}
+          and the{" "}
+          <a
+            href="https://www.iubenda.com/terms-and-conditions/58637640"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Terms and Conditions
+          </a>
+        </div>
+        <div className="flex">
+          <form
+            className="m-auto space-x-4"
+            action={url.loginAction}
+            method="POST"
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              name="accept"
+              type="submit"
+              value="Accept"
+            >
+              <span className="text-lg">Accept</span>
+            </Button>
+            <Button
+              name="cancel"
+              variant="contained"
+              color="secondary"
+              size="large"
+              type="submit"
+              value="Decline"
+            >
+              <span className="text-lg">Decline</span>
+            </Button>
+          </form>
+        </div>
       </div>
-      <div>
-        Please go to the following links to read the{" "}
-        <a
-          href="https://www.iubenda.com/privacy-policy/58637640"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Privacy Policy
-        </a>{" "}
-        and the{" "}
-        <a
-          href="https://www.iubenda.com/terms-and-conditions/58637640"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Terms and Conditions
-        </a>
-      </div>
-      <form className="form-actions" action={url.loginAction} method="POST">
-        <input name="accept" id="kc-accept" type="submit" value="Accept" />
-        <input name="cancel" id="kc-decline" type="submit" value="Decline" />
-      </form>
     </div>
   );
 };
