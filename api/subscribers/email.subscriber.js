aa18fef3c74ef77aeff7a3a3c8eb152c8d14d57b
Gustav Larsson <gustav.larsson@gmail.com>
2021-05-31 10:39:19 +0200

    Initialize keycloak admin client on create-comment in email subscriber

--- api/subscribers/email.subscriber.js
+++ api/subscribers/email.subscriber.js
@@ -1,11 +1,14 @@
 const EmailService = require("../services/EmailService/email.service");
+const initKcAdminClient = require("../utils/initKcAdminClient");
 //const orgHasDiscourse = (org) => org.discourse.url && org.discourse.apiKey;
 
 module.exports = {
-  initialize(eventHub, models, kcAdminClient) {
+  initialize(eventHub, models) {
     eventHub.subscribe(
       "create-comment",
       async ({ currentOrg, currentOrgMember, event, dream, comment }) => {
+        const kcAdminClient = await initKcAdminClient();
+
         await EmailService.sendCommentNotification({
           currentOrg,
           currentOrgMember,
