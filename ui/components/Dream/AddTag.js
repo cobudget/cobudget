d8a3bc305e52644c1ac845d61720ead006ac9c2b
Gustav Larsson <gustav.larsson@gmail.com>
2021-06-03 15:55:15 +0200

    delete tags in component, clear input on tag added, filter out already added tags etc

--- ui/components/Dream/AddTag.js
+++ ui/components/Dream/AddTag.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMutation, gql } from "@apollo/client";
 
 import Downshift from "downshift";
@@ -15,24 +15,26 @@ const ADD_TAG_MUTATION = gql`
   }
 `;
 
-const AddTag = ({ items, dream }) => {
+const AddTag = ({ items: eventTags, dream }) => {
   const [addTag] = useMutation(ADD_TAG_MUTATION);
-
+  const [input, setInput] = useState("");
   return (
     <Downshift
       onChange={(tag) => {
+        if (!tag) return;
         const variables = tag.id ? { tagId: tag.id } : { tagValue: tag.value };
-        console.log({ tag, variables });
+
         addTag({
           variables: { dreamId: dream.id, ...variables },
-        }).then((data) => console.log("Yooo!", data));
+        }).then(() => setInput(""));
       }}
+      onInputValueChange={(value) => setInput(value)}
+      inputValue={input}
       itemToString={(item) => (item ? item.value : "")}
     >
       {({
         getInputProps,
         getItemProps,
-        getLabelProps,
         getMenuProps,
         isOpen,
         inputValue,
@@ -40,46 +42,65 @@ const AddTag = ({ items, dream }) => {
         selectedItem,
         getRootProps,
         openMenu,
+        clearSelection,
       }) => {
-        let filtered = items.filter(
-          (item) =>
-            !inputValue ||
-            item.value.toLowerCase().includes(inputValue.toLowerCase())
+        let filtered = eventTags
+          .filter((tag) => !dream.tags.map((t) => t.id).includes(tag.id))
+          .filter(
+            (item) =>
+              !inputValue ||
+              item.value.toLowerCase().includes(inputValue.toLowerCase())
+          );
+
+        const inputValueAlreadyDefined = filtered.reduce(
+          (previous, current) => previous + (current.value === inputValue),
+          false
         );
-        if (filtered.length === 0) {
-          filtered = [{ value: inputValue }];
+
+        if (inputValue.length && !inputValueAlreadyDefined) {
+          filtered = [{ value: inputValue }, ...filtered];
         }
 
         return (
           <div>
-            <label {...getLabelProps()}>Add a tag</label>
+            {/* <label {...getLabelProps()}>Add a tag</label> */}
             <div
               style={{ display: "inline-block" }}
               {...getRootProps({}, { suppressRefError: true })}
             >
-              <input {...getInputProps({ onFocus: openMenu })} />
+              <input
+                {...getInputProps({ onFocus: openMenu })}
+                className="bg-gray-100 px-2 py-1"
+                placeholder="Add tag"
+                onBlur={() => {
+                  clearSelection();
+                }}
+              />
             </div>
-            <ul {...getMenuProps()}>
-              {isOpen
-                ? filtered.map((item, index) => (
-                    <li
-                      key={item.value}
-                      {...getItemProps({
-                        key: item.value,
-                        index,
-                        item,
-                        style: {
-                          backgroundColor:
-                            highlightedIndex === index ? "lightgray" : "white",
-                          fontWeight: selectedItem === item ? "bold" : "normal",
-                        },
-                      })}
-                    >
-                      {item.value}
-                    </li>
-                  ))
-                : null}
-            </ul>
+
+            {isOpen ? (
+              <ul {...getMenuProps()} className="shadow rounded p-1">
+                {filtered.map((item, index) => (
+                  <li
+                    key={item.value}
+                    {...getItemProps({
+                      key: item.value,
+                      index,
+                      item,
+                      style: {
+                        fontWeight: selectedItem === item ? "bold" : "normal",
+                      },
+                    })}
+                    className={
+                      "rounded px-1 " +
+                      (highlightedIndex === index ? "bg-gray-200" : "")
+                    }
+                  >
+                    {item.value}
+                  </li>
+                ))}
+              </ul>
+            ) : null}
           </div>
         );
       }}
