f82f49dbe77b622441cb89db6e113e1d96c4f8d0
Gustav Larsson <gustav.larsson@gmail.com>
2021-06-04 21:25:17 +0200

    make tag values unique per event

--- api/database/models/tag.js
+++ api/database/models/tag.js
@@ -5,6 +5,6 @@ const TagSchema = new Schema({
   eventId: { type: Schema.Types.ObjectId, required: true, index: true },
   createdAt: { type: Date, required: true, default: Date.now },
   value: { type: String, required: true },
-});
+}).index({ eventId: 1, value: 1 }, { unique: true });
 
 module.exports = TagSchema;
