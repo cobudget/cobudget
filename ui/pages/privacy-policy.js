be569048c60c0b0b20a10b3c4833dcf66b4a7d49
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-06-04 14:39:50 +0200

    Move pp and tos to iframes

--- ui/pages/privacy-policy.js
+++ ui/pages/privacy-policy.js
@@ -1,16 +1,15 @@
-import { useEffect } from "react";
-import HappySpinner from "components/HappySpinner";
-
 const PrivacyPolicy = () => {
-  useEffect(() => {
-    window.location = "https://www.iubenda.com/privacy-policy/58637640";
-  });
-
   return (
     <div className="flex">
-      <div className="flex m-auto mt-12 flex-col items-center">
-        <div className="mb-7">Redirecting to the Privacy Policy</div>
-        <HappySpinner />
+      <div className="flex-1 flex m-auto mt-12 flex-col items-center">
+        <div className="mb-7">Privacy Policy</div>
+        <div className="flex w-full">
+          <iframe
+            className="flex-1 md:mx-16"
+            style={{ height: "70vh" }}
+            src="https://www.iubenda.com/privacy-policy/58637640"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
