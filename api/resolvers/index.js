8c0b19257af8112cf91d30dc428a92a3e3d9c498
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-06-22 21:15:52 +0200

    Randomize order of dreams list, take 2 (#289)

--- api/resolvers/index.js
+++ api/resolvers/index.js
@@ -150,26 +150,29 @@ const resolvers = {
       const tagQuery = {
         ...(tag
           ? {
-              tags: tag,
+              tags: mongoose.Types.ObjectId(tag._id),
             }
           : null),
       };
 
       const adminQuery = {
-        eventId: event.id,
+        eventId: mongoose.Types.ObjectId(event.id),
         ...(textSearchTerm && { $text: { $search: textSearchTerm } }),
         ...tagQuery,
       };
       // todo: create appropriate index for this query
       // if event member, show dreams that are publisehd AND dreams where member is cocreator
       const memberQuery = {
-        eventId: event.id,
-        $or: [{ published: true }, { cocreators: currentEventMember?.id }],
+        eventId: mongoose.Types.ObjectId(event.id),
+        $or: [
+          { published: true },
+          { cocreators: mongoose.Types.ObjectId(currentEventMember?.id) },
+        ],
         ...(textSearchTerm && { $text: { $search: textSearchTerm } }),
         ...tagQuery,
       };
       const othersQuery = {
-        eventId: event.id,
+        eventId: mongoose.Types.ObjectId(event.id),
         published: true,
         ...(textSearchTerm && { $text: { $search: textSearchTerm } }),
         ...tagQuery,
@@ -183,14 +186,36 @@ const resolvers = {
           ? memberQuery
           : othersQuery;
 
+      const userSeed = currentOrgMember
+        ? new Date(currentOrgMember.createdAt).getTime() % 1000
+        : 1;
+
       const dreamsWithExtra = [
-        ...(await Dream.find(query, null, {
-          skip: offset,
-          limit: limit + 1,
-        }).sort({
-          createdAt: -1,
-        })),
-      ];
+        ...(await Dream.aggregate([{ $match: query }])
+          .addFields({
+            position: {
+              $mod: [
+                {
+                  $multiply: [
+                    {
+                      $mod: [
+                        { $toDouble: { $ifNull: ["$createdAt", 1] } },
+                        1000,
+                      ],
+                    },
+                    userSeed,
+                  ],
+                },
+                1000,
+              ],
+            },
+          })
+          .sort({
+            position: 1,
+          })
+          .skip(offset)
+          .limit(limit + 1)),
+      ].map((d) => ({ ...d, id: d._id }));
 
       return {
         moreExist: dreamsWithExtra.length > limit,
