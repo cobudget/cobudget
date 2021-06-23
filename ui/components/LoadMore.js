cecb865c4fcc452da19f7bdcede015aa7fb272f9
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-06-23 21:19:28 +0200

    Fix padding on loadmore button spinner

--- ui/components/LoadMore.js
+++ ui/components/LoadMore.js
@@ -2,19 +2,23 @@ import HappySpinner from "components/HappySpinner";
 import { CheveronDownIcon } from "components/Icons";
 
 const LoadMore = ({ moreExist, loading, onClick }) => {
-  return loading ? (
-    <div className="flex-grow flex justify-center items-center h-64">
-      <HappySpinner />
+  return (
+    <div className="mt-7">
+      {loading ? (
+        <div className="flex-grow flex justify-center items-center">
+          <HappySpinner />
+        </div>
+      ) : moreExist ? (
+        <button
+          className="hover:bg-gray-300 p-1 m-auto h-full rounded flex justify-center items-center focus:outline-none opacity-75"
+          onClick={onClick}
+        >
+          <div>Load more</div>
+          <CheveronDownIcon className="h-8 w-8 ml-3 p-1 text-gray-900 bg-gray-100 rounded-full" />
+        </button>
+      ) : null}
     </div>
-  ) : moreExist ? (
-    <button
-      className="hover:bg-gray-300 p-1 m-auto mt-7 h-full rounded flex justify-center items-center focus:outline-none opacity-75"
-      onClick={onClick}
-    >
-      <div>Load more</div>
-      <CheveronDownIcon className="h-8 w-8 ml-3 p-1 text-gray-900 bg-gray-100 rounded-full" />
-    </button>
-  ) : null;
+  );
 };
 
 export default LoadMore;
