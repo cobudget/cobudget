b4e10f85038ecc8b1cae1306e6552d80d343adc8
Hugi <hugiasgeirsson@gmail.com>
2021-08-13 13:23:16 +0200

    Message to members page when not member of event.

--- ui/pages/[event]/members.js
+++ ui/pages/[event]/members.js
@@ -1,14 +1,27 @@
 import Members from "components/EventMembers";
 import SubMenu from "components/SubMenu";
+import PageHero from "components/PageHero";
+
 const EventMembersPage = ({ event, currentOrgMember }) => {
   const isEventMember =
     currentOrgMember?.currentEventMembership ||
     currentOrgMember?.isOrgAdmin;
-  if (!isEventMember || !event) return null;
-  return (
+  if (!isEventMember || !event) return (
     <div className="flex-1">
       <SubMenu currentOrgMember={currentOrgMember} event={event} />
       <Members event={event} />
+    </div>
+  );
+  return (
+    <div className="flex-1">
+      <SubMenu currentOrgMember={currentOrgMember} event={event} />
+      <PageHero>
+        <div className="flex justify-between">
+          <h2 className="text-2xl font-semibold">
+            You need to be a member of this event to see the member list.
+          </h2>
+        </div>
+      </PageHero>
     </div>
   );
 };
