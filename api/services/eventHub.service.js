884574032db6a8da3e3d97cd6d54fe0cd562c93f
Gustav Larsson <gustav.larsson@gmail.com>
2021-09-16 10:39:15 +0200

    revert eventHub.service.js to previous state

--- api/services/eventHub.service.js
+++ api/services/eventHub.service.js
@@ -1,4 +1,40 @@
 class EventHub {
+  static subscriptions = new Proxy(
+    {},
+    {
+      get: (target, name) => (target.hasOwnProperty(name) ? target[name] : []),
+    }
+  );
+
+  static publish = async (channel, event) => {
+    const results = await Promise.all(
+      this.subscriptions[channel].map(async (fn) => {
+        try {
+          return await fn(event);
+        } catch (error) {
+          return { error };
+        }
+      })
+    );
+
+    const errors = results
+      .map((result) => result.error)
+      .filter((error) => error);
+
+    if (errors.length) {
+      errors.map(console.error);
+      throw new Error(errors.join(", "));
+    }
+
+    return results.reduce(
+      (hash, result) => ({
+        ...result,
+        ...hash,
+      }),
+      {}
+    );
+  };
+
   static subscribe(channel, namespace, fn) {
     this.subscriptions[channel] = this.subscriptions[channel].concat(
       async (...args) => ({
@@ -7,41 +43,5 @@ class EventHub {
     );
   }
 }
-
-EventHub.subscriptions = new Proxy(
-  {},
-  {
-    // not sure what's going on here lol
-    // eslint-disable-next-line no-prototype-builtins
-    get: (target, name) => (target.hasOwnProperty(name) ? target[name] : []),
-  }
-);
-
-EventHub.publish = async (channel, event) => {
-  const results = await Promise.all(
-    this.subscriptions[channel].map(async (fn) => {
-      try {
-        return await fn(event);
-      } catch (error) {
-        return { error };
-      }
-    })
-  );
-
-  const errors = results.map((result) => result.error).filter((error) => error);
-
-  if (errors.length) {
-    errors.map(console.error);
-    throw new Error(errors.join(", "));
-  }
-
-  return results.reduce(
-    (hash, result) => ({
-      ...result,
-      ...hash,
-    }),
-    {}
-  );
-};
 
 module.exports = EventHub;
