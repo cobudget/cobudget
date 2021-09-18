3ba933b1f4ce1bc53a9e31b69290a5629b22e00f
Gustav Larsson <gustav.larsson@gmail.com>
2021-09-18 23:30:38 +0200

    use email provider in next-auth

--- ui/pages/api/auth/[...nextauth].js
+++ ui/pages/api/auth/[...nextauth].js
@@ -1,16 +1,21 @@
 import NextAuth from "next-auth";
-import FacebookProvider from "next-auth/providers/facebook";
+//import FacebookProvider from "next-auth/providers/facebook";
+import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { createContext } from "@sensestack/cobudget-core";
 
-const { prisma } = createContext();
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export default NextAuth({
   providers: [
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_CLIENT_ID,
+    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    // }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
     }),
   ],
-  adapter: PrismaAdapter({ prisma }),
+  adapter: PrismaAdapter(prisma),
 });
