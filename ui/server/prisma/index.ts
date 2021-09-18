732194225d2a14811f71c981c1feb86354defc1d
Gustav Larsson <gustav.larsson@gmail.com>
2021-09-19 00:15:06 +0200

    make prisma types available

--- ui/server/prisma/index.ts
+++ ui/server/prisma/index.ts
@@ -1,6 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma;
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
+
+// @see https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
+let prisma: PrismaClient;
+
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
