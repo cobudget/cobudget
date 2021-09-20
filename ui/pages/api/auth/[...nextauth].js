0dd936954248e436d6e2ce41c9acc44167cc6a8a
Gustav Larsson <gustav.larsson@gmail.com>
2021-09-20 22:05:58 +0200

    remove cookie settings in next-auth

--- ui/pages/api/auth/[...nextauth].js
+++ ui/pages/api/auth/[...nextauth].js
@@ -7,10 +7,6 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const useSecureCookies = process.env.NEXTAUTH_URL.startsWith("https://");
-const cookiePrefix = useSecureCookies ? "__Secure-" : "";
-const hostName = new URL(process.env.NEXTAUTH_URL).hostname;
-
 export default NextAuth({
   providers: [
     // FacebookProvider({
@@ -23,16 +19,4 @@ export default NextAuth({
     }),
   ],
   adapter: PrismaAdapter(prisma),
-  cookies: {
-    sessionToken: {
-      name: `${cookiePrefix}next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: useSecureCookies,
-        domain: hostName == "localhost" ? hostName : "." + hostName, // add a . in front so that subdomains are included
-      },
-    },
-  },
 });
