4192ab238be07ae7c1ed34941081c2e489520f2b
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-09-24 17:12:25 +0200

    Add more error logging in kcadminclient

--- api/utils/initKcAdminClient.js
+++ api/utils/initKcAdminClient.js
@@ -11,26 +11,43 @@ module.exports = async function () {
       `Missing required keycloak admin env var ${process.env.KEYCLOAK_AUTH_SERVER} ${process.env.KEYCLOAK_ADMIN_USERNAME} ${process.env.KEYCLOAK_ADMIN_PASSWORD?.[0]} ${process.env.KEYCLOAK_REALM}`
     );
   }
-  const kcAdminClient = new KcAdminClient({
-    baseUrl: process.env.KEYCLOAK_AUTH_SERVER,
-    realmName: "master",
-    requestConfig: {
-      /* Axios request config options https://github.com/axios/axios#request-config */
-    },
-  });
 
-  // Authorize with username / password
-  await kcAdminClient.auth({
-    username: process.env.KEYCLOAK_ADMIN_USERNAME,
-    password: process.env.KEYCLOAK_ADMIN_PASSWORD,
-    grantType: "password",
-    clientId: "admin-cli",
-    totp: "123456", // optional Time-based One-time Password if OTP is required in authentication flow
-  });
+  let kcAdminClient;
+  try {
+    kcAdminClient = new KcAdminClient({
+      baseUrl: process.env.KEYCLOAK_AUTH_SERVER,
+      realmName: "master",
+      requestConfig: {
+        /* Axios request config options https://github.com/axios/axios#request-config */
+      },
+    });
+  } catch (err) {
+    console.error("kcAdminClient init failed");
+    throw err;
+  }
 
-  kcAdminClient.setConfig({
-    realmName: process.env.KEYCLOAK_REALM,
-  });
+  try {
+    // Authorize with username / password
+    await kcAdminClient.auth({
+      username: process.env.KEYCLOAK_ADMIN_USERNAME,
+      password: process.env.KEYCLOAK_ADMIN_PASSWORD,
+      grantType: "password",
+      clientId: "admin-cli",
+      totp: "123456", // optional Time-based One-time Password if OTP is required in authentication flow
+    });
+  } catch (err) {
+    console.error("kcAdminClient auth failed");
+    throw err;
+  }
+
+  try {
+    kcAdminClient.setConfig({
+      realmName: process.env.KEYCLOAK_REALM,
+    });
+  } catch (err) {
+    console.error("kcAdminClient setConfig failed");
+    throw err;
+  }
 
   return kcAdminClient;
 };
