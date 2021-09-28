ae285bef59db597b7640d8edfc4fa64296eb3496
Gustav Larsson <gustav.larsson@gmail.com>
2021-09-28 23:20:45 +0200

    add extra cobudget org

--- ui/utils/dreamName.js
+++ ui/utils/dreamName.js
@@ -6,7 +6,9 @@ function capitalize(str) {
 // quick hack to change the word dream to bucket for the cobudget org
 export default (org, shouldCapitalize) => {
   const name =
-    org?.subdomain === "cobudget" || org?.subdomain === "grassrootsfund"
+    org?.subdomain === "cobudget" ||
+    org?.subdomain === "grassrootsfund" ||
+    org?.subdomain === "c3"
       ? "bucket"
       : "dream";
   if (shouldCapitalize) return capitalize(name);
