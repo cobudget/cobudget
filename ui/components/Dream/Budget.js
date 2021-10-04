36c694275f948d0d5bce0296c929e10d835940ab
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-10-04 13:30:19 +0200

    Update budget ux/copy (#341)
    * Expenses -> Costs
    
    * Split out nonmonetary contribs
    
    * Add clarification what the funding goal means
    
    * Change gap class
    
    * Update display of budget totals

--- ui/components/Dream/Budget.js
+++ ui/components/Dream/Budget.js
@@ -18,7 +18,16 @@ const DreamBudget = ({
 }) => {
   const [editing, setEditing] = useState(false);
   const incomeItems = budgetItems.filter((item) => item.type === "INCOME");
+  const monetaryIncome = incomeItems.filter((item) => item.min > 0);
+  const nonMonetaryIncome = incomeItems.filter((item) => item.min === 0);
   const expenseItems = budgetItems.filter((item) => item.type === "EXPENSE");
+
+  const expenseTotalMin = minGoal / 100;
+  const expenseTotalMax = maxGoal / 100;
+  const incomeTotal = monetaryIncome.map((e) => e.min).reduce((a, b) => a + b);
+  const goalTotalMin = expenseTotalMin - incomeTotal;
+  const goalTotalMax = expenseTotalMax - incomeTotal;
+
   return (
     <>
       {editing && (
@@ -49,7 +58,7 @@ const DreamBudget = ({
           </div>
           {expenseItems.length > 0 && (
             <>
-              <h3 className="font-lg font-medium mb-2">Expenses</h3>
+              <h3 className="font-lg font-medium mb-2">Costs</h3>
 
               <div className="mb-8 rounded shadow overflow-hidden bg-gray-100">
                 <table className="table-fixed w-full">
@@ -65,26 +74,68 @@ const DreamBudget = ({
                         </td>
                       </tr>
                     ))}
+                    <tr
+                      key="total"
+                      className="bg-gray-200 border-t-2 border-gray-300"
+                    >
+                      <td className="px-4 py-2">Total</td>
+                      <td className="px-4 py-2">
+                        {thousandSeparator(expenseTotalMin)} -{" "}
+                        {thousandSeparator(expenseTotalMax)} {currency}
+                      </td>
+                    </tr>
                   </tbody>
                 </table>
               </div>
             </>
           )}
-          {incomeItems.length > 0 && (
+          {monetaryIncome.length > 0 && (
+            <>
+              <h3 className="font-lg font-medium mb-2">Existing funds</h3>
+
+              <div className="mb-8 rounded shadow overflow-hidden bg-gray-100">
+                <table className="table-fixed w-full">
+                  <tbody>
+                    {monetaryIncome.map((budgetItem) => (
+                      <tr
+                        key={budgetItem.id}
+                        className="bg-gray-100 even:bg-white"
+                      >
+                        <td className="px-4 py-2">{budgetItem.description}</td>
+                        <td className="px-4 py-2">
+                          {thousandSeparator(budgetItem.min)} {currency}
+                        </td>
+                      </tr>
+                    ))}
+                    <tr
+                      key="total"
+                      className="bg-gray-200 border-t-2 border-gray-300"
+                    >
+                      <td className="px-4 py-2">Total</td>
+                      <td className="px-4 py-2">
+                        {thousandSeparator(incomeTotal)} {currency}
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </>
+          )}
+          {nonMonetaryIncome.length > 0 && (
             <>
               <h3 className="font-lg font-medium mb-2">
-                Existing funding and resources
+                Non-monetary contributions
               </h3>
 
               <div className="mb-8 rounded shadow overflow-hidden bg-gray-100">
                 <table className="table-fixed w-full">
                   <tbody>
-                    {incomeItems.map((budgetItem, i) => (
-                      <tr key={i} className="bg-gray-100 even:bg-white">
+                    {nonMonetaryIncome.map((budgetItem) => (
+                      <tr
+                        key={budgetItem.id}
+                        className="bg-gray-100 even:bg-white"
+                      >
                         <td className="px-4 py-2">{budgetItem.description}</td>
-                        <td className="px-4 py-2">
-                          {thousandSeparator(budgetItem.min)} {currency}
-                        </td>
                       </tr>
                     ))}
                   </tbody>
@@ -92,14 +143,27 @@ const DreamBudget = ({
               </div>
             </>
           )}
-          <p className="font-lg font-medium mb-2">
-            Total funding goal: {thousandSeparator(minGoal / 100)} {currency}{" "}
-            {maxGoal > 0 && (
-              <>
-                (stretch goal: {thousandSeparator(maxGoal / 100)} {currency})
-              </>
-            )}
-          </p>
+          <div className="text-lg font-medium mb-2 gap-x-4 gap-y-2 flex flex-wrap justify-between">
+            <div>
+              <div className="font-bold">Funding goal:</div>
+              <div className="text-base">= Costs - Existing funds</div>
+            </div>
+            <div className="self-end">
+              <span className="font-bold">
+                {thousandSeparator(goalTotalMin)} {currency}
+              </span>
+              {maxGoal > 0 && (
+                <>
+                  {" "}
+                  (stretch goal:{" "}
+                  <span className="font-bold">
+                    {thousandSeparator(goalTotalMax)} {currency}
+                  </span>
+                  )
+                </>
+              )}
+            </div>
+          </div>
         </div>
       ) : canEdit ? (
         <button
