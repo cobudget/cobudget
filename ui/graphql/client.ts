5e470c5a2e090b00ff0446d21ad5d6256da1cad3
Gustav Larsson <gustav.larsson@gmail.com>
2021-10-08 00:17:46 +0200

    add urql client

---
+++ ui/graphql/client.ts
@@ -0,0 +1,32 @@
+import { dedupExchange, fetchExchange } from "urql";
+import { NextUrqlContext, SSRExchange } from "next-urql";
+import { devtoolsExchange } from "@urql/devtools";
+
+export const getUrl = (): string => {
+  if (process.browser) return `/api`;
+
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}/api`;
+  }
+
+  if (process.env.NODE_ENV === `development`)
+    return `http://localhost:3000/api`;
+
+  return "https://cobudget.com/api";
+};
+
+export const client = (
+  ssrExchange: SSRExchange,
+  ctx: NextUrqlContext | undefined
+) => {
+  return {
+    url: getUrl(),
+    exchanges: [devtoolsExchange, dedupExchange, ssrExchange, fetchExchange],
+    fetchOptions: {
+      headers: {
+        ...ctx?.req?.headers,
+      },
+      credentials: "include",
+    },
+  };
+};
--- ui/pages/_app.js
+++ ui/pages/_app.js
@@ -5,9 +5,11 @@ import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import { UserProvider, useFetchUser } from "lib/user";
 import "../styles.css";
 import "react-tippy/dist/tippy.css";
-
+import { withUrqlClient, initUrqlClient } from "next-urql";
+import { client } from "../graphql/client";
 import Layout from "../components/Layout";
 import Modal from "../components/Modal";
+import * as Urql from "urql";
 
 export const TOP_LEVEL_QUERY = gql`
   query TopLevelQuery($slug: String!, $orgSlug: String!) {
@@ -106,80 +108,37 @@ export const TOP_LEVEL_QUERY = gql`
   }
 `;
 
-const theme = createTheme({
-  overrides: {
-    MuiButton: {
-      contained: {
-        borderRadius: "6px",
-        backgroundColor: "white",
-        "&:hover": {
-          backgroundColor: "white",
-        },
-      },
-    },
-  },
-  typography: {
-    button: {
-      textTransform: "none",
-    },
-    fontFamily: [
-      "Inter",
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-    fontSize: 14,
-  },
-  shadows: [
-    "none",
-    "0px 2px 1px -1px rgba(63, 75, 90,0.1),0px 1px 1px 0px rgba(63, 75, 90,0.08),0px 1px 3px 0px rgba(63, 75, 90,0.06)",
-    "0px 3px 8px -2px rgba(63, 75, 90,0.1),0px 2px 2px 0px rgba(63, 75, 90,0.08),0px 1px 5px 0px rgba(63, 75, 90,0.06)",
-    "0px 3px 3px -2px rgba(63, 75, 90,0.1),0px 3px 4px 0px rgba(63, 75, 90,0.08),0px 1px 8px 0px rgba(63, 75, 90,0.06)",
-    "0px 2px 8px -1px rgba(63, 75, 90,0.1),0px 4px 5px 0px rgba(63, 75, 90,0.08),0px 1px 10px 0px rgba(63, 75, 90,0.06)",
-    "0px 3px 5px -1px rgba(63, 75, 90,0.1),0px 5px 8px 0px rgba(63, 75, 90,0.08),0px 1px 14px 0px rgba(63, 75, 90,0.06)",
-    "0px 3px 5px -1px rgba(63, 75, 90,0.1),0px 6px 10px 0px rgba(63, 75, 90,0.08),0px 1px 18px 0px rgba(63, 75, 90,0.06)",
-    "0px 4px 5px -2px rgba(63, 75, 90,0.1),0px 7px 10px 1px rgba(63, 75, 90,0.08),0px 2px 16px 1px rgba(63, 75, 90,0.06)",
-    "0px 5px 5px -3px rgba(63, 75, 90,0.1),0px 8px 10px 1px rgba(63, 75, 90,0.08),0px 3px 14px 2px rgba(63, 75, 90,0.06)",
-    "0px 5px 6px -3px rgba(63, 75, 90,0.1),0px 9px 12px 1px rgba(63, 75, 90,0.08),0px 3px 16px 2px rgba(63, 75, 90,0.06)",
-    "0px 6px 6px -3px rgba(63, 75, 90,0.1),0px 10px 14px 1px rgba(63, 75, 90,0.08),0px 4px 18px 3px rgba(63, 75, 90,0.06)",
-    "0px 6px 7px -4px rgba(63, 75, 90,0.1),0px 11px 15px 1px rgba(63, 75, 90,0.08),0px 4px 20px 3px rgba(63, 75, 90,0.06)",
-    "0px 7px 8px -4px rgba(63, 75, 90,0.1),0px 12px 17px 2px rgba(63, 75, 90,0.08),0px 5px 22px 4px rgba(63, 75, 90,0.06)",
-    "0px 7px 8px -4px rgba(63, 75, 90,0.1),0px 13px 19px 2px rgba(63, 75, 90,0.08),0px 5px 24px 4px rgba(63, 75, 90,0.06)",
-    "0px 7px 9px -4px rgba(63, 75, 90,0.1),0px 14px 21px 2px rgba(63, 75, 90,0.08),0px 5px 26px 4px rgba(63, 75, 90,0.06)",
-    "0px 8px 9px -5px rgba(63, 75, 90,0.1),0px 15px 22px 2px rgba(63, 75, 90,0.08),0px 6px 28px 5px rgba(63, 75, 90,0.06)",
-    "0px 8px 10px -5px rgba(63, 75, 90,0.1),0px 16px 24px 2px rgba(63, 75, 90,0.08),0px 6px 30px 5px rgba(63, 75, 90,0.06)",
-    "0px 8px 11px -5px rgba(63, 75, 90,0.1),0px 17px 26px 2px rgba(63, 75, 90,0.08),0px 6px 32px 5px rgba(63, 75, 90,0.06)",
-    "0px 9px 11px -5px rgba(63, 75, 90,0.1),0px 18px 28px 2px rgba(63, 75, 90,0.08),0px 7px 34px 6px rgba(63, 75, 90,0.06)",
-    "0px 9px 12px -6px rgba(63, 75, 90,0.1),0px 19px 29px 2px rgba(63, 75, 90,0.08),0px 7px 36px 6px rgba(63, 75, 90,0.06)",
-    "0px 10px 13px -6px rgba(63, 75, 90,0.1),0px 20px 31px 3px rgba(63, 75, 90,0.08),0px 8px 38px 7px rgba(63, 75, 90,0.06)",
-    "0px 10px 13px -6px rgba(63, 75, 90,0.1),0px 21px 33px 3px rgba(63, 75, 90,0.08),0px 8px 40px 7px rgba(63, 75, 90,0.06)",
-    "0px 10px 14px -6px rgba(63, 75, 90,0.1),0px 22px 35px 3px rgba(63, 75, 90,0.08),0px 8px 42px 7px rgba(63, 75, 90,0.06)",
-    "0px 11px 14px -7px rgba(63, 75, 90,0.1),0px 23px 36px 3px rgba(63, 75, 90,0.08),0px 9px 44px 8px rgba(63, 75, 90,0.06)",
-    "0px 11px 15px -7px rgba(63, 75, 90,0.1),0px 24px 38px 3px rgba(63, 75, 90,0.08),0px 9px 46px 8px rgba(63, 75, 90,0.06)",
-  ],
-});
+export const CURRENT_USER_QUERY = gql`
+  {
+    currentUser {
+      id
+      username
+      email
+    }
+  }
+`;
+
+export function useGetCurrentUserQuery(options = {}) {
+  return Urql.useQuery({ query: CURRENT_USER_QUERY, ...options });
+}
 
 const MyApp = ({ Component, pageProps, router }) => {
-  const { user, loading } = useFetchUser();
-
+  // const { user, loading } = useFetchUser();
+  const [
+    { data: { currentUser } = { currentUser: null }, fetching },
+  ] = useGetCurrentUserQuery();
+  console.log({ currentUser, fetching });
   useEffect(() => {
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles && jssStyles.parentNode)
       jssStyles.parentNode.removeChild(jssStyles);
   });
-
-  const {
-    data: { currentUser, currentOrg, currentOrgMember, event } = {},
-  } = useQuery(TOP_LEVEL_QUERY, {
-    variables: { slug: router.query.event, orgSlug: router.query.organization },
-  });
+  let currentOrg, currentOrgMember, event;
+  // const {
+  //   data: { currentUser, currentOrg, currentOrgMember, event } = {},
+  // } = useQuery(TOP_LEVEL_QUERY, {
+  //   variables: { slug: router.query.event },
+  // });
 
   const [modal, setModal] = useState(null);
 
@@ -192,43 +151,47 @@ const MyApp = ({ Component, pageProps, router }) => {
   };
 
   return (
-    <UserProvider value={{ user, loading }}>
-      <ThemeProvider theme={theme}>
-        <Modal
-          active={modal}
-          closeModal={closeModal}
-          currentOrgMember={currentOrgMember}
-          currentUser={currentUser}
-          currentOrg={currentOrg}
-        />
-        <Layout
+    // <UserProvider value={{ user, loading }}>
+    // <ThemeProvider theme={theme}>
+    <>
+      <Modal
+        active={modal}
+        closeModal={closeModal}
+        currentOrgMember={currentOrgMember}
+        currentUser={currentUser}
+        currentOrg={currentOrg}
+      />
+      <Layout
+        currentUser={currentUser}
+        currentOrgMember={currentOrgMember}
+        currentOrg={currentOrg}
+        openModal={openModal}
+        event={event}
+        router={router}
+        title={
+          currentOrg
+            ? event
+              ? `${event.title} | ${currentOrg.name}`
+              : currentOrg.name
+            : "Plato"
+        }
+      >
+        <Component
+          {...pageProps}
+          event={event}
           currentUser={currentUser}
           currentOrgMember={currentOrgMember}
           currentOrg={currentOrg}
           openModal={openModal}
-          event={event}
           router={router}
-          title={
-            currentOrg
-              ? event
-                ? `${event.title} | ${currentOrg.name}`
-                : currentOrg.name
-              : "Plato"
-          }
-        >
-          <Component
-            {...pageProps}
-            event={event}
-            currentUser={currentUser}
-            currentOrgMember={currentOrgMember}
-            currentOrg={currentOrg}
-            openModal={openModal}
-            router={router}
-          />
-        </Layout>
-      </ThemeProvider>
-    </UserProvider>
+        />
+      </Layout>
+    </>
+    // </ThemeProvider>
+    // </UserProvider>
   );
 };
 
-export default withApollo({ ssr: true })(MyApp);
+export default withUrqlClient(client, { ssr: true })(MyApp);
+
+//export default withApollo({ ssr: true })(MyApp);
