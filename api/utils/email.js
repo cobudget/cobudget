37fb193f6fe7c5577d35acace091e9a20dc599f3
Gustav Larsson <gustav.larsson@gmail.com>
2021-11-08 11:47:30 +0100

    remove legacy api

--- api/utils/email.js
+++
@@ -1,8 +0,0 @@
-const isValidEmail = (inputEmail) => {
-  const email = inputEmail.toLowerCase();
-  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  return emailRegex.test(email);
-};
-
-module.exports = { isValidEmail };
