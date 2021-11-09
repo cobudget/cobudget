c12cb89e31233c79d87f2861babfb9ba842ca088
Gustav Larsson <gustav.larsson@gmail.com>
2021-11-09 20:06:14 +0100

    Fix redirect from create page

--- ui/pages/create.js
+++ ui/pages/create.js
@@ -2,7 +2,7 @@ import EditOrganization from "../components/Org/EditOrganization";
 
 const CreateOrganizationPage = ({ currentUser }) => {
   if (!currentUser) {
-    //if (typeof window !== "undefined") window.location.assign("/api/login"); //TODO: fix
+    if (typeof window !== "undefined") window.location.assign("/login"); //TODO: fix redirect after the fact
     return (
       <h1 className="page flex-grow flex justify-center items-center text-xl">
         Redirecting to login/signup...
