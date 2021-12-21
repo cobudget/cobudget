26e0162bcc134ac94aa26563cdb560d23daa421f
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-12-21 18:11:21 +0100

    Send notification emails to commenters

--- ui/server/services/EmailService/email.service.ts
+++ ui/server/services/EmailService/email.service.ts
@@ -1,15 +1,26 @@
 import { SendEmailInput, sendEmails } from "server/send-email";
 import isURL from "validator/lib/isURL";
-import escape from "validator/lib/escape";
+import escapeImport from "validator/lib/escape";
+import { uniqBy } from "lodash";
 import prisma from "../../prisma";
+import { orgHasDiscourse } from "server/subscribers/discourse.subscriber";
 
 /** path including leading slash */
 function appLink(path: string): string {
   const protocol = process.env.NODE_ENV == "production" ? "https" : "http";
   const url = `${protocol}://${process.env.DEPLOY_URL}${path}`;
-  if (!isURL(url)) throw new Error(`Invalid link in mail: ${url}`);
+  if (!isURL(url, { host_whitelist: [process.env.DEPLOY_URL.split(":")[0]] }))
+    throw new Error(`Invalid link in mail: ${url}`);
   return url;
 }
+
+function escape(input: string): string | undefined | null {
+  // sometimes e.g. usernames are null atm
+  if (input === null || typeof input === "undefined") return input;
+  return escapeImport(input);
+}
+
+const footer = `<i>Cobudget helps groups collaboratively ideate, gather and distribute funds to projects that matter to them. <a href="https://guide.cobudget.co/">Discover how it works.</a></i>`;
 
 export default class EmailService {
   static async sendCommentNotification({
@@ -29,7 +40,7 @@ export default class EmailService {
       `/${currentOrg?.slug ?? "c"}/${event.slug}/${dream.id}`
     );
 
-    const emails: SendEmailInput[] = cocreators
+    const cocreatorEmails: SendEmailInput[] = cocreators
       .filter(
         (collectionMember) => collectionMember.id !== currentCollMember.id
       )
@@ -47,12 +58,53 @@ export default class EmailService {
           <br/><br/>
           <a href="${bucketLink}">Have a look</a>
           <br/><br/>
-          <i>Cobudget helps groups collaboratively ideate, gather and distribute funds to projects that matter to them. <a href="https://guide.cobudget.co/">Discover how it works.</a></i>
+          ${footer}
           `,
         })
       );
 
-    await sendEmails(emails);
+    await sendEmails(cocreatorEmails);
+
+    if (!orgHasDiscourse(currentOrg)) {
+      const comments = await prisma.comment.findMany({
+        where: { bucketId: dream.id },
+        include: {
+          collMember: {
+            include: {
+              user: true,
+            },
+          },
+        },
+      });
+      const commenters = uniqBy(
+        comments
+          .map((comment) => comment.collMember.user)
+          .filter((user) => currentUser.id !== user.id)
+          // don't email the cocreators, we just emailed them above
+          .filter(
+            (user) =>
+              !cocreators
+                .map((cocreator) => cocreator.user.id)
+                .includes(user.id)
+          ),
+        "id"
+      );
+      const commenterEmails = commenters.map((recipient) => ({
+        to: recipient.email,
+        subject: `New comment by ${currentUser.name} in bucket ${dream.title}`,
+        html: `Hey ${escape(recipient.name)}!
+          <br/><br/>
+          People are talking about “${escape(
+            dream.title
+          )}” - <a href="${bucketLink}">have a look at the new comments</a>.
+          <br/><br/>
+          "${escape(comment.content)}"
+          <br/><br/>
+          ${footer}
+        `,
+      }));
+      await sendEmails(commenterEmails);
+    }
   }
 
   static async sendRequestToJoinNotifications(
