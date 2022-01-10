54aabfc8163aa7faf91cfce1c3cba16b03f73bd7
Gustav Larsson <gustav.larsson@gmail.com>
2022-01-11 00:05:52 +0100

    update comments context to use bucketId

--- ui/components/Bucket/Comments/AddComment.js
+++ ui/components/Bucket/Comments/AddComment.js
@@ -12,9 +12,13 @@ function AddComment() {
   const [submitting, setSubmitting] = useState(false);
   const { handleSubmit, register, errors } = useForm();
   const inputRef = useRef();
-  const { addComment, dream, collection, currentOrg, currentUser } = useContext(
-    Context
-  );
+  const {
+    addComment,
+    bucketId,
+    collection,
+    currentOrg,
+    currentUser,
+  } = useContext(Context);
   if (
     currentOrg?.discourseUrl &&
     !currentUser.currentOrgMember?.hasDiscourseApiKey
@@ -31,7 +35,7 @@ function AddComment() {
     <form
       onSubmit={handleSubmit(() => {
         setSubmitting(true);
-        addComment({ bucketId: dream.id, content })
+        addComment({ bucketId, content })
           .then(() => {
             inputRef.current.blur();
             setContent("");
