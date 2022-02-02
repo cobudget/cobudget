056336a8d910bd85e0d63a57c6d64a05650933fe
Gustav Larsson <gustav.larsson@gmail.com>
2022-02-02 18:07:13 +0100

    Send emails with postmark library

--- ui/server/send-email.ts
+++ ui/server/send-email.ts
@@ -1,6 +1,6 @@
-import nodemailer from "nodemailer";
-import postmarkTransport from "nodemailer-postmark-transport";
-
+//import nodemailer from "nodemailer";
+//import postmarkTransport from "nodemailer-postmark-transport";
+import { Client } from "postmark";
 export interface SendEmailInput {
   to: string;
   subject: string;
@@ -8,21 +8,7 @@ export interface SendEmailInput {
   html?: string;
 }
 
-// in dev we send over local smtp, set up an app like Mailhog to catch it
-const client =
-  process.env.NODE_ENV === "development"
-    ? nodemailer.createTransport({
-        host: "localhost",
-        port: 1025,
-        secure: false,
-      })
-    : nodemailer.createTransport(
-        postmarkTransport({
-          auth: {
-            apiKey: process.env.POSTMARK_API_TOKEN,
-          },
-        })
-      );
+const client = new Client(process.env.POSTMARK_API_TOKEN);
 
 const send = async (mail: SendEmailInput) => {
   if (process.env.NODE_ENV === "development") {
@@ -31,18 +17,16 @@ const send = async (mail: SendEmailInput) => {
         mail.text ?? mail.html
       }\n`
     );
-  }
-
-  try {
-    await client.sendMail({
-      from: process.env.FROM_EMAIL,
-      to: mail.to,
-      subject: mail.subject,
-      text: mail.text,
-      html: mail.html,
-    });
-  } catch (err) {
-    if (process.env.NODE_ENV !== "development") {
+  } else {
+    try {
+      await client.sendEmail({
+        From: process.env.FROM_EMAIL,
+        To: mail.to,
+        Subject: mail.subject,
+        TextBody: mail.text,
+        HtmlBody: mail.html,
+      });
+    } catch (err) {
       throw err;
     }
   }
