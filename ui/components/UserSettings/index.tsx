687f6e6cddecb4823f2f54696e7213367ea1857d
Jacob Karlsson <jacob.karlsson95@gmail.com>
2022-02-15 17:45:30 +0100

    Improve styling

--- ui/components/UserSettings/index.tsx
+++ ui/components/UserSettings/index.tsx
@@ -1,6 +1,6 @@
 import { useQuery, gql, useMutation } from "urql";
 import { omit } from "lodash";
-import { Checkbox } from "@material-ui/core";
+import { Checkbox, FormControlLabel, FormGroup } from "@material-ui/core";
 import HappySpinner from "components/HappySpinner";
 
 const USER_SETTINGS_QUERY = gql`
@@ -28,7 +28,7 @@ const labels = {
     "Another comment is made in a bucket I have previously commented in",
   allocatedToYou: "When I'm allocated money that I can contribute to buckets",
   refundedBecauseBucketCancelled:
-    "I get refunded money because a bucket I contributed cancelled its funding",
+    "I get refunded money because a bucket I contributed to cancelled its funding",
   bucketPublishedInRound:
     "A new bucket is published in a round I'm a member of",
   contributionToYourBucket:
@@ -46,16 +46,18 @@ const EmailSettingItem = ({ settingKey, value }) => {
   }
 
   return (
-    <div>
-      {labels[settingKey]}
-      <Checkbox
-        onChange={(e) =>
-          setEmailSetting({ settingKey, value: e.target.checked })
-        }
-        checked={value}
-        disabled={fetching}
-      />
-    </div>
+    <FormControlLabel
+      control={
+        <Checkbox
+          onChange={(e) =>
+            setEmailSetting({ settingKey, value: e.target.checked })
+          }
+          checked={value}
+          disabled={fetching}
+        />
+      }
+      label={labels[settingKey]}
+    />
   );
 };
 
@@ -72,11 +74,13 @@ const SettingsIndex = () => {
   const emailSettings = omit(data.currentUser.emailSettings, ["id", "userId"]);
 
   return (
-    <div>
-      Please send me an email when:
-      {Object.entries(emailSettings).map(([settingKey, value]) => (
-        <EmailSettingItem settingKey={settingKey} value={value} />
-      ))}
+    <div className="page">
+      <FormGroup className="max-w-xl mx-auto p-5 space-y-2 bg-white rounded-lg shadow">
+        <div>Please send me an email when:</div>
+        {Object.entries(emailSettings).map(([settingKey, value]) => (
+          <EmailSettingItem settingKey={settingKey} value={value} />
+        ))}
+      </FormGroup>
     </div>
   );
 };
