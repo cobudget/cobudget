25156801bf0830ee349077246ed7070db2697d6f
Jacob Karlsson <jacob.karlsson95@gmail.com>
2022-05-04 18:19:45 +0200

    Add buttons for going into editing

--- ui/components/Bucket/DirectFunding.tsx
+++ ui/components/Bucket/DirectFunding.tsx
@@ -1,7 +1,11 @@
 import { useQuery, useMutation, gql } from "urql";
 import { useRouter } from "next/router";
 import toast from "react-hot-toast";
+import { Tooltip } from "react-tippy";
 import HappySpinner from "components/HappySpinner";
+import IconButton from "components/IconButton";
+import { EditIcon } from "components/Icons";
+import { useState } from "react";
 
 const BUCKET_QUERY = gql`
   query Bucket($id: ID!) {
@@ -28,6 +32,8 @@ const DirectFunding = ({ canEdit = false, round }) => {
   const router = useRouter();
   const bucketId = router.query.bucket;
 
+  const [editing, setEditing] = useState(false);
+
   const [{ data, fetching: fetchingQuery, error }] = useQuery({
     query: BUCKET_QUERY,
     variables: { id: bucketId },
@@ -37,6 +43,7 @@ const DirectFunding = ({ canEdit = false, round }) => {
     EDIT_BUCKET_MUTATION
   );
 
+  // we don't show this section at all to people who aren't admins/cocreators. they'll interact through the Fund button instead
   if (!canEdit || !round.directFundingEnabled) return null;
 
   if (fetchingQuery) return <HappySpinner />;
@@ -49,7 +56,26 @@ const DirectFunding = ({ canEdit = false, round }) => {
   const bucket = data.bucket;
 
   return bucket.directFundingEnabled ? (
-    <div>Direct funding is enabled for this bucket</div>
+    <div>
+      <h2 className="text-xl font-medium mb-2">Direct funding</h2>
+      <div className="flex justify-between">
+        <div>
+          <div className="font-medium">Terms</div>
+          {editing ? (
+            <div>Now we are editing thiiiiings</div>
+          ) : (
+            <div>Funds received are donations{/*TODO: make dynamic*/}</div>
+          )}
+        </div>
+        {!editing && (
+          <Tooltip title="Edit Direct funding" position="bottom" size="small">
+            <IconButton onClick={() => setEditing(true)}>
+              <EditIcon className="h-6 w-6" />
+            </IconButton>
+          </Tooltip>
+        )}
+      </div>
+    </div>
   ) : (
     <button
       disabled={fetchingMutation}
