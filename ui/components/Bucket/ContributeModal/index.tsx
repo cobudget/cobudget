9c31aa8a1e148c6799b1d713ce315d3e120b39e7
Jacob Karlsson <jacob.karlsson95@gmail.com>
2022-05-11 18:52:51 +0200

    Add tabs to contrib modal

--- ui/components/Bucket/ContributeModal/index.tsx
+++ ui/components/Bucket/ContributeModal/index.tsx
@@ -1,6 +1,21 @@
 import { Modal } from "@material-ui/core";
+import { Tab } from "@headlessui/react";
 import { FormattedMessage } from "react-intl";
 import FromBalance from "./FromBalance";
+
+const StyledTab = ({ children, color }) => (
+  <Tab
+    className={({ selected }) =>
+      `block px-2 py-4 border-b-2 font-medium transition-colors ${
+        selected
+          ? `border-${color} text-anthracit`
+          : "border-transparent text-gray-500"
+      }`
+    }
+  >
+    {children}
+  </Tab>
+);
 
 const ContributeModal = ({ handleClose, bucket, currentUser }) => {
   return (
@@ -13,11 +28,28 @@ const ContributeModal = ({ handleClose, bucket, currentUser }) => {
         <h1 className="text-2xl mb-2 font-semibold">
           <FormattedMessage defaultMessage="Contribute to" /> {bucket.title}
         </h1>
-        <FromBalance
-          currentUser={currentUser}
-          bucket={bucket}
-          handleClose={handleClose}
-        />
+        <Tab.Group
+          defaultIndex={currentUser.currentCollMember.balance > 0 ? 0 : 1}
+        >
+          <Tab.List className="space-x-2 max-w-screen-xl mx-auto flex px-2 overflow-x-auto mb-3">
+            <StyledTab color={bucket.round.color}>
+              <FormattedMessage defaultMessage="From my balance" />
+            </StyledTab>
+            <StyledTab color={bucket.round.color}>
+              <FormattedMessage defaultMessage="Direct with card" />
+            </StyledTab>
+          </Tab.List>
+          <Tab.Panels>
+            <Tab.Panel>
+              <FromBalance
+                currentUser={currentUser}
+                bucket={bucket}
+                handleClose={handleClose}
+              />
+            </Tab.Panel>
+            <Tab.Panel>Item two</Tab.Panel>
+          </Tab.Panels>
+        </Tab.Group>
       </div>
     </Modal>
   );
