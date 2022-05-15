be97482f9fd08e8f659275ccad6783342f5d6c70
Jacob Karlsson <jacob.karlsson95@gmail.com>
2022-05-15 22:25:59 +0200

    Add tip input

--- ui/components/Bucket/ContributeModal/WithCard.tsx
+++ ui/components/Bucket/ContributeModal/WithCard.tsx
@@ -1,15 +1,32 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { FormattedMessage } from "react-intl";
+import { Prisma } from "@prisma/client";
 import Markdown from "components/Markdown";
 import TextField from "components/TextField";
+import SelectInput from "components/SelectInput";
 
-const Title = ({ children }) => <div className="font-medium">{children}</div>;
+const Decimal = Prisma.Decimal;
+
+const Title = ({ children }) => (
+  <div className="font-medium mt-5">{children}</div>
+);
 
 const WithCard = ({ bucket }) => {
   const isExchange = bucket.directFundingType === "EXCHANGE";
 
   const [contribution, setContribution] = useState<number>(
     isExchange ? bucket.exchangeMinimumContribution / 100 : 0
+  );
+  // fraction
+  const [cobudgetTip, setCobudgetTip] = useState<number>(0.15);
+
+  const tipAlternatives = useMemo(
+    () =>
+      [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3].map((fraction) => ({
+        fraction,
+        amount: new Decimal(fraction).mul(contribution || 0),
+      })),
+    [contribution]
   );
 
   return (
@@ -39,10 +56,27 @@ const WithCard = ({ bucket }) => {
       />
       {isExchange && contribution * 100 < bucket.exchangeMinimumContribution && (
         <div className="text-red-600">
-          Contribution needs to be at least{" "}
+          <FormattedMessage defaultMessage="Contribution needs to be at least" />{" "}
           {bucket.exchangeMinimumContribution / 100} {bucket.round.currency}
         </div>
       )}
+      <Title>
+        <FormattedMessage defaultMessage="Add a tip to support Cobudget" />
+      </Title>
+      <SelectInput
+        fullWidth
+        value={cobudgetTip}
+        onChange={(e) => setCobudgetTip(e.target.value)}
+      >
+        {tipAlternatives.map(({ fraction, amount }, i) => {
+          return (
+            // TODO: truncate to 2 decimals
+            <option value={fraction} key={fraction}>
+              {amount.toString()} {bucket.round.currency} ({fraction * 100}%)
+            </option>
+          );
+        })}
+      </SelectInput>
     </div>
   );
 };
