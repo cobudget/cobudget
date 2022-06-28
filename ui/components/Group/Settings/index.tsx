67d5e4d864afc9a8d47d94142b319a640ef2f4a1
Gustav Larsson <gustav.larsson@gmail.com>
2022-06-28 18:26:44 +0200

    Update structure of group settings and add billing portal link

---
+++ ui/components/Group/Settings/index.tsx
@@ -0,0 +1,81 @@
+import { useMemo } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useIntl } from "react-intl";
+
+import capitalize from "utils/capitalize";
+
+// import CustomFields from "./CustomFields";
+import GeneralSettings from "./GeneralSettings";
+import Billing from "./Billing";
+// import Guidelines from "./Guidelines";
+// import Granting from "./Granting";
+// import Tags from "./Tags";
+// import BucketReview from "./BucketReview";
+// import Discourse from "./Discourse";
+
+const GroupSettings = ({
+  settingsTabSlug,
+  currentUser,
+  group,
+}: {
+  settingsTabSlug: string;
+  round: any;
+  currentUser: any;
+  group: any;
+}) => {
+  const intl = useIntl();
+  const router = useRouter();
+
+  const tabs = useMemo(
+    () => [
+      {
+        slug: "",
+        name: intl.formatMessage({ defaultMessage: "General" }),
+        component: GeneralSettings,
+      },
+      {
+        slug: "billing",
+        name: intl.formatMessage({ defaultMessage: "Billing" }),
+        component: Billing,
+      },
+    ],
+    [intl]
+  );
+
+  const currentTab =
+    tabs.find((tab) => tab.slug === settingsTabSlug) ?? tabs[0];
+
+  const SettingsComponent = currentTab.component;
+
+  return (
+    <div className="page">
+      <div className="grid sm:grid-cols-6">
+        <div className="flex flex-col mb-4">
+          {tabs.map((tab) => (
+            <Link
+              key={tab.name}
+              href={`/${router.query.group}/settings/${tab.slug}`}
+            >
+              <a
+                className={
+                  "text-left p-2 focus:outline-none font-medium " +
+                  (settingsTabSlug === tab.slug
+                    ? "text-black"
+                    : "text-gray-500")
+                }
+              >
+                {tab.name}
+              </a>
+            </Link>
+          ))}
+        </div>
+        <div className="py-6 col-span-4 bg-white rounded-lg shadow overflow-hidden">
+          <SettingsComponent group={group} currentUser={currentUser} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GroupSettings;
---
+++ ui/pages/api/stripe/create-stripe-billing-portal-url.ts
@@ -0,0 +1,34 @@
+import handler from "server/api-handler";
+import prisma from "server/prisma";
+import stripe from "server/stripe";
+import { getRequestOrigin } from "server/get-request-origin";
+
+export default handler().get(async (req, res) => {
+    if (!req.user) throw new Error("You must be logged in");
+
+    const groupId = req.query.groupId;
+    // expect groupId to be a string
+    if (typeof groupId !== "string") throw new Error("groupId missing or not a string");
+
+    const origin = getRequestOrigin(req);
+    const group = await prisma.group.findUnique({ where: { id: groupId } });
+    if (!group.stripeCustomerId) throw new Error("Group has no customer id");
+
+    //check that user is admin of group
+    const groupMember = await prisma.groupMember.findFirst({
+        where: {
+            groupId,
+            userId: req.user.id
+        }
+    });
+    if (!groupMember.isAdmin) throw new Error("You need to be admin to access billing portal");
+
+    const { url } = await stripe.billingPortal.sessions.create({
+        customer: group.stripeCustomerId,
+        return_url: `${origin}/${group.slug}/settings/billing`,
+    });
+
+    res.redirect(303, url);
+});
+
+
