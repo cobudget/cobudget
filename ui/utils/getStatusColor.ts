5e119ff19e218c9859d9a5f0c649f0a9486e3651
Jacob Karlsson <jacob.karlsson95@gmail.com>
2022-09-25 20:01:00 +0200

    Comment out unreachable code

--- ui/utils/getStatusColor.ts
+++ ui/utils/getStatusColor.ts
@@ -1,7 +1,8 @@
 const getStatusColor = (status: string) => {
   if (status === "PENDING_APPROVAL") return "bg-app-gray";
   else if (status === "") return "bg-app-yellow";
-  else if (status === "") return "bg-app-red";
+  //TODO: can't reach here, why?
+  //else if (status === "") return "bg-app-red";
   else if (status === "OPEN_FOR_FUNDING") return "bg-app-orange";
   else if (status === "FUNDED") return "bg-app-purple";
   else if (status === "COMPLETED") return "bg-app-green";
