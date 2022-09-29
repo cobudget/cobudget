e53d44f9b4bebbbc95a185c72be3958a066ede6a
Jacob Karlsson <jacob.karlsson95@gmail.com>
2022-09-29 13:49:38 +0200

    Localize new strings

--- ui/components/Fallback.tsx
+++ ui/components/Fallback.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FormattedMessage } from "react-intl";
 
 function Fallback() {
   const handleReload = () => {
@@ -11,17 +12,16 @@ function Fallback() {
     <div className="flex w-screen h-screen content-center justify-center mt-2">
       <div className="w-1/2 h-1/4">
         <h2 className="text-gray-500 font-medium leading-tight text-4xl mt-0 mb-2">
-          Unexpected Error
+          <FormattedMessage defaultMessage="Unexpected Error" />
         </h2>
         <p className="text-gray-600">
-          The app encountered an unexpected error. We have noted the isse and we
-          will fix it shortly. Sorry for the inconvenience.
+          <FormattedMessage defaultMessage="The app encountered an unexpected error. We have noted the isse and we will fix it shortly. Sorry for the inconvenience." />
         </p>
         <button
           className="mt-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
           onClick={handleReload}
         >
-          Reload
+          <FormattedMessage defaultMessage="Reload" />
         </button>
       </div>
     </div>
