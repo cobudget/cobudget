59a89428e525d34933d84fc65f6f826497654b59
nauroz <alinauroze@hotmail.com>
2023-02-25 23:53:43 +0500

    integration settings page

---
+++ ui/components/RoundSettings/Integrations/index.tsx
@@ -0,0 +1,60 @@
+import { Divider, List } from "@material-ui/core";
+import HappySpinner from "components/HappySpinner";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { FormattedMessage, useIntl } from "react-intl";
+import { gql, useQuery } from "urql";
+import SettingsListItem from "../Granting/SettingsListItem";
+
+const GET_ROUND_INTEGRATIONS = gql`
+  query GetRoundIntegrations($roundSlug: String!) {
+    round(roundSlug: $roundSlug) {
+      color
+    }
+  }
+`;
+
+function Integrations() {
+  const router = useRouter();
+  const [{ data, error, fetching }] = useQuery({
+    query: GET_ROUND_INTEGRATIONS,
+    variables: { roundSlug: router.query.round },
+  });
+  const [openModal, setOpenModal] = useState("");
+  const intl = useIntl();
+
+  const round = data?.round;
+
+  if (fetching) {
+    return <HappySpinner />;
+  }
+
+  if (round) {
+    return (
+      <div className="-mb-6">
+        <h2 className="text-2xl font-semibold mb-3 px-6">
+          <FormattedMessage defaultMessage="Integrations" />
+        </h2>
+        <div className="border-t">
+          <List>
+            <SettingsListItem
+              primary={intl.formatMessage({
+                defaultMessage: "Connect round to Open Collective",
+              })}
+              secondary={"ABC"}
+              isSet={false}
+              canEdit={true}
+              openModal={() => setOpenModal("SET_CURRENCY")}
+              roundColor={round.color}
+            />
+          </List>
+          <Divider />
+        </div>
+      </div>
+    );
+  }
+
+  return null;
+}
+
+export default Integrations;
