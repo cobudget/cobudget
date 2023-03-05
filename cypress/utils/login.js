629c70359821e0224c1f60dbc6c536e8de04ad53
nauroz <alinauroze@hotmail.com>
2023-03-05 23:27:22 +0500

    sleep using shell

--- cypress/utils/login.js
+++ cypress/utils/login.js
@@ -3,7 +3,6 @@ import generateToken from "./generateToken";
 export default function () {
   const token = generateToken({ destination: Cypress.env("email") });
   const magicLink = "api/auth/magiclink/callback?token=" + token;
-  cy.wait(30000);
   cy.visit(magicLink, { timeout: 60000 });
   cy.url().should("be.equal", `${Cypress.config("baseUrl")}`);
 }
