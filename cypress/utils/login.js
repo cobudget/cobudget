2452b4e78392f7dbffc696e20bb85335a6947efd
nauroz <alinauroze@hotmail.com>
2023-03-05 22:55:54 +0500

    timeout

--- cypress/utils/login.js
+++ cypress/utils/login.js
@@ -3,6 +3,6 @@ import generateToken from "./generateToken";
 export default function () {
   const token = generateToken({ destination: Cypress.env("email") });
   const magicLink = "api/auth/magiclink/callback?token=" + token;
-  cy.visit(magicLink);
+  cy.visit(magicLink, { timeout: 60000 });
   cy.url().should("be.equal", `${Cypress.config("baseUrl")}`);
 }
