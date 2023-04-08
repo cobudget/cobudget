15322e18488e51bb7c699b173c2b16709b20451e
nauroz <alinauroze@hotmail.com>
2023-04-08 20:46:30 +0500

    loading

--- ui/components/Bucket/Expenses/UploadAttachment.tsx
+++ ui/components/Bucket/Expenses/UploadAttachment.tsx
@@ -1,5 +1,5 @@
 import IconButton from "components/IconButton";
-import { AddIcon, DeleteIcon } from "components/Icons";
+import { AddIcon, DeleteIcon, LoaderIcon } from "components/Icons";
 import React, { useRef, useState } from "react";
 import { FormattedMessage } from "react-intl";
 import uploadImageFiles from "utils/uploadImageFiles";
@@ -8,13 +8,14 @@ import { FileUploader } from "react-drag-drop-files";
 function UploadAttachment({ name, cloudinaryPreset, inputRef }) {
   const fileInputField = useRef(null);
   const [link, setLink] = useState();
+  const [uploading, setUploading] = useState(false);
 
   const removeLink = () => setLink(undefined);
 
   const handleUpload = (file) =>
     uploadImageFiles({
       files: [file],
-      setUploadingImages: [() => ""],
+      setUploadingImages: [setUploading],
       setImages: [
         (fileUrl) => {
           setLink(fileUrl);
@@ -48,11 +49,21 @@ function UploadAttachment({ name, cloudinaryPreset, inputRef }) {
         <div className="flex min-w-0 my-1">
           <button
             type="button"
-            onClick={() => fileInputField.current.click()}
+            disabled={uploading}
+            onClick={(e) => e.preventDefault()}
             className="font-medium flex px-8 py-3 rounded-md  bg-gray-100 w-full border-3 border-transparent"
           >
             <span className="mx-1 my-0.5">
-              <AddIcon width={20} height={20} />
+              {uploading ? (
+                <LoaderIcon
+                  className="animate-spin"
+                  fill="#000"
+                  width={20}
+                  height={20}
+                />
+              ) : (
+                <AddIcon width={20} height={20} />
+              )}
             </span>
             <FormattedMessage defaultMessage="Upload Attachment" />
           </button>
