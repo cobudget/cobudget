45e268f124b50cc134039b49a5b0e94f71f0de78
nauroz <alinauroze@hotmail.com>
2023-04-11 23:02:19 +0500

    use zero width space

--- ui/utils/parseMDSource.ts
+++ ui/utils/parseMDSource.ts
@@ -1,6 +1,6 @@
 const parseMDSource = (source: string | null) => {
   return (source || "")
-    .replace(/\n/gi, "\n &nbsp;")
+    .replace(/\n/gi, "\n &#8203;")
     .replace(/\n&#8203;>/gi, "\n >")
     .replace(/\n&#8203;*/gi, "\n");
 };
