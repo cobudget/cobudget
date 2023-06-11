6bc9e9f7336ab9f44f0782e32c7b5bd83e5e4330
Hugi <hugiasgeirsson@gmail.com>
2023-06-11 20:51:39 +0200

    Update login.tsx (debug magic link limit)

--- ui/pages/login.tsx
+++ ui/pages/login.tsx
@@ -18,7 +18,7 @@ function Login({ fbLoginEnabled, googleLoginEnabled }) {
     <div className="page">
       <Toaster />
       <h1 className="mt-10 text-gray-700 text-center text-xl font-medium">
-        Log in to {process.env.PLATFORM_NAME}
+        Log in to {process.env.PLATFORM_NAME} (limit to one email per {parseInt(process.env.MAGIC_LINK_TIME_LIMIT) / 60} minutes).
       </h1>
 
       <div className="max-w-sm bg-white mx-auto my-6 p-6 shadow rounded">
