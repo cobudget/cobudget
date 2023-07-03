ae69c26c145968c64a76c6112b7a0c1b958955c8
nauroz <alinauroze@hotmail.com>
2023-07-04 00:51:38 +0500

    lint

--- ui/utils/sorting.ts
+++ ui/utils/sorting.ts
@@ -1,13 +1,13 @@
 export const arraySortByStringKey = (arr, key: string) => {
-    const newArr: typeof arr = [...arr];
-    newArr.sort(( a, b ) => {
-        if ( a[key].toLowerCase() < b[key].toLowerCase() ){
-          return -1;
-        }
-        if ( a[key].toLowerCase() > b[key].toLowerCase() ){
-          return 1;
-        }
-        return 0;
-      })
-    return newArr as typeof arr;
-}
+  const newArr = [...arr];
+  newArr.sort((a, b) => {
+    if (a[key].toLowerCase() < b[key].toLowerCase()) {
+      return -1;
+    }
+    if (a[key].toLowerCase() > b[key].toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  });
+  return newArr;
+};
