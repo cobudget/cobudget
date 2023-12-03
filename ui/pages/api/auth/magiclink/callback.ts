a8c94507e271d1a9d46bb555f7029e1c832f159e
nauroz <alinauroze@hotmail.com>
2023-12-03 13:56:41 +0500

    save code

--- ui/pages/api/auth/magiclink/callback.ts
+++ ui/pages/api/auth/magiclink/callback.ts
@@ -1,7 +1,14 @@
 import passport from "passport";
 import handler from "../../../../server/api-handler";
+import jwt from "jsonwebtoken";
 
 export default handler()
+  .use((req, res, next) => {
+    const { token } = req.query;
+    const payload = jwt.verify(token, process.env.MAGIC_LINK_SECRET);
+    console.log(payload);
+    next();
+  })
   .use(passport.authenticate("magiclogin"))
   .use((req, res) => {
     res.redirect(req.user?.redirect || "/");
