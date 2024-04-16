2124c3a3d5bb0f5e7421189186c3c15457cc9b3b
or313 <orbarl313@gmail.com>
2024-04-16 11:39:11 +0300

    Disable captcha

--- ui/pages/api/auth/magiclink/index.ts
+++ ui/pages/api/auth/magiclink/index.ts
@@ -2,25 +2,25 @@ import handler from "../../../../server/api-handler";
 import magicLink from "../../../../server/passport/magicLink";
 
 export default handler().post(async (req: any, res: any) => {
-  const { captchaToken } = req.body;
+  // const { captchaToken } = req.body;
 
-  if (!captchaToken) {
-    res.status(400).send({ error: "No captcha token" });
-    return;
-  }
+  // if (!captchaToken) {
+  //   res.status(400).send({ error: "No captcha token" });
+  //   return;
+  // }
 
-  const captchaVerificationResponse = await fetch(
-    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captchaToken}`,
-    {
-      method: "POST",
-    }
-  );
-  const captchaVerification = await captchaVerificationResponse.json();
+  // const captchaVerificationResponse = await fetch(
+  //   `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captchaToken}`,
+  //   {
+  //     method: "POST",
+  //   }
+  // );
+  // const captchaVerification = await captchaVerificationResponse.json();
 
-  if (captchaVerification.success === true) {
+  // if (captchaVerification.success === true) {
     return magicLink.send(req, res);
-  } else {
-    res.status(400).send({ error: "Invalid captcha token" });
-    return;
-  }
+  // } else {
+  //   res.status(400).send({ error: "Invalid captcha token" });
+  //   return;
+  // }
 });
